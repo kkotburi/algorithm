6b2e30cc3cee975abdf972e0dd69f03ced991639
kkotburi <128236313+kkotburi@users.noreply.github.com>
2023-07-27 10:27:25 +0900

    [unrated] Title: 홀수 vs 짝수, Time: 0.11 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181887. 홀수 vs 짝수/홀수 vs 짝수.js
+++ 프로그래머스/unrated/181887. 홀수 vs 짝수/홀수 vs 짝수.js
@@ -1,13 +1,12 @@
 const solution = (num_list) => {
-  let output_1 = 0;
-  let output_2 = 0;
+  let oddSum = 0;
+  let evenSum = 0;
   for (let i = 0; i <= num_list.length - 1; i++) {
     if (i % 2 === 0 && i >= 0) {
-      output_1 += num_list[i];
-    }
-    if (i % 2 !== 0 && i >= 0) {
-      output_2 += num_list[i];
+      oddSum += num_list[i];
+    } else {
+      evenSum += num_list[i];
     }
   }
-  return output_1 > output_2 ? output_1 : output_2;
+  return oddSum > evenSum ? oddSum : evenSum;
 };
