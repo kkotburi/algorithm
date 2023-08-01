38039dfa4292f22173f6458f1a3932644016418c
kkotburi <128236313+kkotburi@users.noreply.github.com>
2023-08-01 10:21:46 +0900

    [unrated] Title: 문자열이 몇 번 등장하는지 세기, Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181871. 문자열이 몇 번 등장하는지 세기/문자열이 몇 번 등장하는지 세기.js
+++ 프로그래머스/unrated/181871. 문자열이 몇 번 등장하는지 세기/문자열이 몇 번 등장하는지 세기.js
@@ -1,7 +1,7 @@
 const solution = (myString, pat) => {
   let newString = [];
 
-  for (let i = 0; i < myString.length - pat.length + 1; i++) {
+  for (let i = 0; i <= myString.length - pat.length; i++) {
     let string = myString.substr(i, pat.length);
     if (string === pat) {
       newString.push(string);
@@ -9,6 +9,5 @@ const solution = (myString, pat) => {
   }
 
   const answer = newString.length;
-
   return answer;
 };
