5821bf734754dc358c65d86e6dc0f999bc284ce3
kkotburi <128236313+kkotburi@users.noreply.github.com>
2023-08-03 23:10:54 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120861. 캐릭터의 좌표/캐릭터의 좌표.js
+++ 프로그래머스/lv0/120861. 캐릭터의 좌표/캐릭터의 좌표.js
@@ -2,24 +2,22 @@ const solution = (keyinput, board) => {
   let x = 0;
   let y = 0;
 
-  const maxX = (board[0] - 1) / 2;
-  const minX = -((board[0] - 1) / 2);
-  const maxY = (board[1] - 1) / 2;
-  const minY = -((board[1] - 1) / 2);
+  const boardX = (board[0] - 1) / 2;
+  const boardY = (board[1] - 1) / 2;
 
   for (let i = 0; i < keyinput.length; i++) {
     switch (keyinput[i]) {
       case "right":
-        x < maxX && x++;
+        x < boardX && x++;
         break;
       case "up":
-        y < maxY && y++;
+        y < boardY && y++;
         break;
       case "left":
-        x > minX && x--;
+        x > -boardX && x--;
         break;
       case "down":
-        y > minY && y--;
+        y > -boardY && y--;
         break;
     }
   }
