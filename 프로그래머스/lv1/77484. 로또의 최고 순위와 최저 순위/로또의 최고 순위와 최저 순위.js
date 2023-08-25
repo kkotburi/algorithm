2bad80a70ff0a2c3794031340f5e4c0a39d84467
kkotburi <128236313+kkotburi@users.noreply.github.com>
2023-08-25 10:22:56 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.08 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
+++ 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
@@ -1,14 +1,10 @@
 const solution = (lottos, win_nums) => {
-  const intersection = win_nums.filter((win_num) =>
-    lottos.includes(win_num)
-  ).length;
-  const unkwon = lottos.filter((lotto) => lotto === 0).length;
+  const rank = [6, 6, 5, 4, 3, 2, 1];
 
-  // 최대 개수
-  let caseWin =
-    unkwon === 0 && intersection === 0 ? 6 : 7 - (unkwon + intersection);
-  // 최소 개수
-  let caseLose = intersection === 0 ? 6 : 7 - intersection;
+  const unknown = lottos.filter((lotto) => lotto === 0).length;
+  const intersection = win_nums.filter((num) => lottos.includes(num)).length;
 
-  return [caseWin, caseLose];
+  const caseWin = unknown + intersection;
+
+  return [rank[caseWin], rank[intersection]];
 };
