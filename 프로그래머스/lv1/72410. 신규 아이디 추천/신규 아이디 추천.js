ab466a640baffbd4985ec9f8086db18a919f37cd
kkotburi <128236313+kkotburi@users.noreply.github.com>
2023-09-30 02:21:19 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.23 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.js
+++ 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.js
@@ -1,33 +1,19 @@
 const solution = (new_id) => {
-  if (/^[a-z0-9-_]([\.]?[a-z0-9-_]){3,15}$/.test(new_id)) {
+  if (/^[a-z0-9-_](?!.*[.]{2,})[a-z0-9-_.]{1,13}[a-z0-9]$/.test(new_id)) {
     return new_id;
   } else {
     let answer = "";
-
-    answer = new_id.toLowerCase();
-
-    const extraction = new RegExp(/[a-z0-9-_\.]/, "g");
-    answer = answer.match(extraction).join("");
-
-    answer = answer.replace(/\.+/g, ".");
-
-    if (answer[0] === ".") {
-      answer = answer.slice(1);
-    }
-
-    answer = answer.replace(/^$/, "a");
-
-    if (answer.length > 15) {
-      answer = answer.slice(0, 15);
-    }
-    if (answer[answer.length - 1] === ".") {
-      answer = answer.slice(0, answer.length - 1);
-    }
-
+    answer = new_id
+      .toLowerCase()
+      .replace(/[^\w-_.]/g, "")
+      .replace(/\.+/g, ".")
+      .replace(/^\.|\.$/g, "")
+      .replace(/^$/, "a")
+      .slice(0, 15)
+      .replace(/\.$/, "");
     while (answer.length < 3) {
       answer += answer[answer.length - 1];
     }
-
-    return answer === "undefined" ? "aa" : answer;
+    return answer;
   }
 };
