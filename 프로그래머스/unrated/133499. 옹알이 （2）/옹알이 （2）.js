27e6f26d39aa66df9080efb754624c1c5365560f
kkotburi <128236313+kkotburi@users.noreply.github.com>
2023-10-03 00:04:46 +0900

    [unrated] Title: 옹알이 (2), Time: 0.30 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.js
+++ 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.js
@@ -1,23 +1,11 @@
 const solution = (babbling) => {
   let answer = 0;
-  const possibleList = ["aya", "ye", "woo", "ma"];
-  const impossibleList = ["ayaaya", "yeye", "woowoo", "mama"];
 
   for (i = 0; i < babbling.length; i++) {
     if (
-      !impossibleList.filter((impossible) => babbling[i].includes(impossible))
-        .length
+      !/(aya|ye|woo|ma)\1+/.test(babbling[i]) &&
+      /^(aya|ye|woo|ma)+$/.test(babbling[i])
     ) {
-      for (j = 0; j < possibleList.length; j++) {
-        if (babbling[i]) {
-          babbling[i] = babbling[i].replaceAll(possibleList[j], "0");
-        } else {
-          break;
-        }
-      }
-    }
-
-    if (!babbling[i].replaceAll("0", "")) {
       answer++;
     }
   }
