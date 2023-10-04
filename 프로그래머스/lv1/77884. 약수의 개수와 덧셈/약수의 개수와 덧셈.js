ce53b681d30e9e890ba8a0805def19d9e850f185
kkotburi <128236313+kkotburi@users.noreply.github.com>
2023-10-05 00:12:45 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.20 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
@@ -0,0 +1,7 @@
+const solution = (left, right) => {
+  let answer = 0;
+  for (let i = left; i <= right; i++) {
+    Number.isInteger(Math.sqrt(i)) ? (answer -= i) : (answer += i);
+  }
+  return answer;
+};
