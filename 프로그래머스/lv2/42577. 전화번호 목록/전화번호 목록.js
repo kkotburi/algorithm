e950572a044b7095460ee7607ea6335c2a15d41e
kkotburi <128236313+kkotburi@users.noreply.github.com>
2023-10-09 00:07:54 +0900

    [level 2] Title: 전화번호 목록, Time: 124.81 ms, Memory: 75.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.js
@@ -0,0 +1,11 @@
+const solution = (phone_book) => {
+  let answer = true;
+  let sortedPhone_book = phone_book.sort();
+  for (let i = 0; i < sortedPhone_book.length - 1; i++) {
+    if (!phone_book[i + 1].indexOf(phone_book[i])) {
+      answer = false;
+      break;
+    }
+  }
+  return answer;
+};
