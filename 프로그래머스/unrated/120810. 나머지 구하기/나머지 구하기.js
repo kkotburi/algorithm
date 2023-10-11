e4de34c6552e38e99d9c31f148d7ca4649443f9a
kkotburi <128236313+kkotburi@users.noreply.github.com>
2023-10-11 22:02:30 +0900

    [level unrated] Title: 나머지 구하기, Time: 0.02 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120810. 나머지 구하기/나머지 구하기.js
+++ 프로그래머스/unrated/120810. 나머지 구하기/나머지 구하기.js
@@ -1,7 +1,1 @@
-const solution = (num1, num2) => {
-  let answer = num1;
-  for (let i = 1; i <= num1 / num2; i++) {
-    answer -= num2;
-  }
-  return answer;
-};
+const solution = (num1, num2) => num1 % num2;
