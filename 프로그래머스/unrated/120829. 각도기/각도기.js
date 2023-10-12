017049d93d07f969e6bb8dde34d6e5248ed0a059
kkotburi <128236313+kkotburi@users.noreply.github.com>
2023-10-12 23:33:48 +0900

    [level unrated] Title: 각도기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120829. 각도기/각도기.js
@@ -0,0 +1,11 @@
+const solution = (angle) => {
+  let answer = 4;
+  if (angle < 90) {
+    answer = 1;
+  } else if (angle === 90) {
+    answer = 2;
+  } else if (angle < 180) {
+    answer = 3;
+  }
+  return answer;
+};
