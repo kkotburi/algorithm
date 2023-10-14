b8252b32028634690ca83d6e649274189051979c
kkotburi <128236313+kkotburi@users.noreply.github.com>
2023-10-15 06:07:50 +0900

    [level 2] Title: [3차] n진수 게임, Time: 0.72 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.js
+++ 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.js
@@ -1,12 +1,12 @@
 const solution = (n, t, m, p) => {
-  let number = "";
-  for (let i = 0; p + (t - 1) * m > number.length; i++) {
-    number += i.toString(n);
+  let numbers = "";
+  for (let i = 0; p + (t - 1) * m > numbers.length; i++) {
+    numbers += i.toString(n);
   }
   let answer = "";
   for (let i = 0; t > answer.length; i++) {
-    if (number[p - 1 + m * i]) {
-      answer += number[p - 1 + m * i].toUpperCase();
+    if (numbers[p - 1 + m * i]) {
+      answer += numbers[p - 1 + m * i].toUpperCase();
     }
   }
   return answer;
