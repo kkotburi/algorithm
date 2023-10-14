e8cd1d1534145edbfea9a73eb9f521ac607e99e8
kkotburi <128236313+kkotburi@users.noreply.github.com>
2023-10-14 21:04:31 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.js
@@ -0,0 +1,20 @@
+const solution = (n, k) => {
+  let answer = 0;
+  let array = n
+    .toString(k)
+    .split("0")
+    .filter((one) => one && one !== "1");
+  let isDecimal = 0;
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 2; j <= Math.sqrt(array[i]); j++) {
+      if (Number.isInteger(array[i] / j)) {
+        isDecimal = array[i] / j;
+        break;
+      }
+    }
+    if (!isDecimal) {
+      answer++;
+    }
+  }
+  return answer;
+};
