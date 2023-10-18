c12cf19db2e60dee0ae7586f35f5bfcc1eeeb1a3
kkotburi <128236313+kkotburi@users.noreply.github.com>
2023-10-19 04:45:59 +0900

    [level unrated] Title: 배열 뒤집기, Time: 0.02 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/120821. 배열 뒤집기/배열 뒤집기.js
+++ 프로그래머스/unrated/120821. 배열 뒤집기/배열 뒤집기.js
@@ -1,7 +1,1 @@
-const solution = (num_list) => {
-  let answer = [];
-  for (let i = num_list.length - 1; i >= 0; i--) {
-    answer.push(num_list[i]);
-  }
-  return answer;
-};
+const solution = (num_list) => num_list.reverse();
