d449f0531500e61de7047868318325ff32e9ddca
kkotburi <128236313+kkotburi@users.noreply.github.com>
2023-10-25 09:36:59 +0900

    [level unrated] Title: 배열 원소의 길이, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120854. 배열 원소의 길이/배열 원소의 길이.js
+++ 프로그래머스/unrated/120854. 배열 원소의 길이/배열 원소의 길이.js
@@ -1,7 +1,1 @@
-const solution = (strlist) => {
-  let answer = [];
-  for (let i = 0; i < strlist.length; i++) {
-    answer.push(strlist[i].length);
-  }
-  return answer;
-};
+const solution = (strlist) => strlist.map((str) => str.length);
