9d2c6b6804238bd9eb887054c3924a235a1f5afb
kkotburi <128236313+kkotburi@users.noreply.github.com>
2023-11-15 21:28:24 +0900

    [level unrated] Title: A 강조하기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181874. A 강조하기/A 강조하기.js
+++ 프로그래머스/unrated/181874. A 강조하기/A 강조하기.js
@@ -1,11 +1,1 @@
-const solution = (myString) => {
-  let answer = "";
-  for (let i = 0; i < myString.length; i++) {
-    if (myString[i].toLowerCase() === "a") {
-      answer += "A";
-    } else {
-      answer += myString[i].toLowerCase();
-    }
-  }
-  return answer;
-};
+const solution = (myString) => myString.toLowerCase().replaceAll("a", "A");
