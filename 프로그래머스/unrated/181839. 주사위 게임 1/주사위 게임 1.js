1464dca5660725d06ea5901e51b9cd544fec618c
kkotburi <128236313+kkotburi@users.noreply.github.com>
2023-11-23 16:27:14 +0900

    [level unrated] Title: 주사위 게임 1, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181839. 주사위 게임 1/주사위 게임 1.js
@@ -0,0 +1,11 @@
+const solution = (a, b) => {
+  let answer = 0;
+  if (a % 2 && b % 2) {
+    answer = a ** 2 + b ** 2;
+  } else if (a % 2 || b % 2) {
+    answer = 2 * (a + b);
+  } else {
+    answer = Math.abs(a - b);
+  }
+  return answer;
+};
