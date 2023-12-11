a1956eb08e5a660d612208c72a4ce2e1c4e7f8cb
kkotburi <128236313+kkotburi@users.noreply.github.com>
2023-12-11 22:37:54 +0900

    [level unrated] Title: 문자열 바꿔서 찾기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.js
@@ -0,0 +1,11 @@
+const solution = (myString, pat) => {
+  let answer = "";
+  for (let i = 0; i < myString.length; i++) {
+    if (myString[i] === "A") {
+      answer += "B";
+    } else {
+      answer += "A";
+    }
+  }
+  return answer.includes(pat) ? 1 : 0;
+};
