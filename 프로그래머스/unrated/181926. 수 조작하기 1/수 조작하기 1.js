e0718d4c45e1348c752d3567e2232783adc238ce
kkotburi <128236313+kkotburi@users.noreply.github.com>
2023-12-26 23:28:18 +0900

    [level unrated] Title: 수 조작하기 1, Time: 4.74 ms, Memory: 36.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181926. 수 조작하기 1/수 조작하기 1.js
@@ -0,0 +1,18 @@
+const solution = (n, control) => {
+  let answer = n;
+  for (let i = 0; i < control.length; i++) {
+    if (control[i] === "w") {
+      answer += 1;
+    }
+    if (control[i] === "s") {
+      answer -= 1;
+    }
+    if (control[i] === "d") {
+      answer += 10;
+    }
+    if (control[i] === "a") {
+      answer -= 10;
+    }
+  }
+  return answer;
+};
