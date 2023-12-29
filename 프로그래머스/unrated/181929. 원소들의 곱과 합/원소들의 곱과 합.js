a6be7f0c1b6c741ba9b367e0f04c55ae0d51aa9b
kkotburi <128236313+kkotburi@users.noreply.github.com>
2023-12-29 17:12:42 +0900

    [level unrated] Title: 원소들의 곱과 합, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181929. 원소들의 곱과 합/원소들의 곱과 합.js
@@ -0,0 +1,10 @@
+const solution = (num_list) => {
+  let answer = 0;
+  let sum = 0;
+  let times = 1;
+  for (let i = 0; i < num_list.length; i++) {
+    sum += num_list[i];
+    times *= num_list[i];
+  }
+  return times < sum ** 2 ? 1 : 0;
+};
