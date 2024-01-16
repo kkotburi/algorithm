71236ca04de76bda76c69da32921024a43d06e13
kkotburi <128236313+kkotburi@users.noreply.github.com>
2024-01-16 16:01:58 +0900

    [level Lv.0] Title: 가까운 1 찾기, Time: 0.06 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.0/181898. 가까운 1 찾기/가까운 1 찾기.js
@@ -0,0 +1,12 @@
+const solution = (arr, idx) => {
+  let answer = 0;
+  for (let i = idx; i <= arr.length; i++) {
+    if (arr[i]) {
+      answer = i;
+      break;
+    } else {
+      answer = -1;
+    }
+  }
+  return answer;
+};
