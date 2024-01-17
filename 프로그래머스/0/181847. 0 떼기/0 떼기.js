28301e27cfc192b65498fa4f115eab34f023aa6c
kkotburi <128236313+kkotburi@users.noreply.github.com>
2024-01-17 15:44:40 +0900

    [level 0] Title: 0 떼기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.js
@@ -0,0 +1,10 @@
+const solution = (n_str) => {
+  let answer = "";
+  for (let i = 0; i < n_str.length; i++) {
+    if (n_str[i] !== "0") {
+      answer += n_str.slice(i);
+      break;
+    }
+  }
+  return answer;
+};
