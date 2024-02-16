62d8a7438198495c91124591a47ea50128b00511
kkotburi <128236313+kkotburi@users.noreply.github.com>
2024-02-16 18:30:24 +0900

    [level 0] Title: 이차원 배열 대각선 순회하기, Time: 0.19 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181829. 이차원 배열 대각선 순회하기/이차원 배열 대각선 순회하기.js
@@ -0,0 +1,9 @@
+const solution = (board, k) => {
+  let answer = 0;
+  for (let i = 0; i <= k && i < board.length; i++) {
+    for (let j = 0; j <= k - i && j < board[0].length; j++) {
+      answer += board[i][j];
+    }
+  }
+  return answer;
+};
