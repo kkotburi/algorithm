43e8213d2843b827953bf97e4e7521c54f684025
kkotburi <128236313+kkotburi@users.noreply.github.com>
2024-02-17 21:52:56 +0900

    [level 0] Title: 수열과 구간 쿼리 1, Time: 2.16 ms, Memory: 36.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.js
@@ -0,0 +1,8 @@
+const solution = (arr, queries) => {
+  for (let i = 0; i < queries.length; i++) {
+    for (let j = queries[i][0]; j <= queries[i][1]; j++) {
+      arr[j]++;
+    }
+  }
+  return arr;
+};
