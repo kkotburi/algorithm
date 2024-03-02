b46385e3dd30076cee105c958cffe4d4a2583d67
kkotburi <128236313+kkotburi@users.noreply.github.com>
2024-03-02 23:38:26 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
+++ 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
@@ -1,8 +1,5 @@
-const solution = (my_string) => {
-  my_string = my_string.replace(/[a-z]/gi, "a").split("a");
-  answer = 0;
-  for (let i = 0; i < my_string.length; i++) {
-    answer += +my_string[i];
-  }
-  return answer;
-};
+const solution = (my_string) =>
+  (my_string = my_string
+    .replace(/[a-z]/gi, " ")
+    .split(" ")
+    .reduce((a, c) => a + Number(c), 0));
