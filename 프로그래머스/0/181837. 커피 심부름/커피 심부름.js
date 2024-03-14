d49b6312c487a5b58f423d3b94ebc423e2ca81f5
kkotburi <128236313+kkotburi@users.noreply.github.com>
2024-03-15 01:56:45 +0900

    [level 0] Title: 커피 심부름, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181837. 커피 심부름/커피 심부름.js
@@ -0,0 +1,7 @@
+const solution = (order) => {
+  let answer = 0;
+  for (let i = 0; i < order.length; i++) {
+    order[i].includes("cafelatte") ? (answer += 5000) : (answer += 4500);
+  }
+  return answer;
+};
