e244e547652743c7c5f63f4d6164b9893a69c76f
kkotburi <128236313+kkotburi@users.noreply.github.com>
2024-03-21 22:52:36 +0900

    [level 0] Title: 배열 만들기 4, Time: 2.67 ms, Memory: 37 MB -BaekjoonHub

---
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.js
@@ -0,0 +1,12 @@
+const solution = (arr) => {
+  let stk = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (stk.length === 0 || stk[stk.length - 1] < arr[i]) {
+      stk.push(arr[i]);
+    } else {
+      stk.pop();
+      i--;
+    }
+  }
+  return stk;
+};
