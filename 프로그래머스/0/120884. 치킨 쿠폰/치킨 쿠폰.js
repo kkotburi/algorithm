9670359c6655a0bcaa14679590030291ade06fd8
kkotburi <128236313+kkotburi@users.noreply.github.com>
2024-04-12 23:58:22 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.02 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.js
+++ 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.js
@@ -1,9 +1,1 @@
-const solution = (chicken) => {
-  let answer = parseInt(chicken / 10);
-  let remain = parseInt(chicken / 10) + (chicken % 10);
-  while (remain >= 10) {
-    answer += parseInt(remain / 10);
-    remain = parseInt(remain / 10) + (remain % 10);
-  }
-  return answer;
-};
+const solution = (chicken) => parseInt((chicken - 1) / 9);
