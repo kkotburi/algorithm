629a66095484fd4d042534ff462c9b60c5a72c8f
kkotburi <128236313+kkotburi@users.noreply.github.com>
2024-04-14 00:22:43 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.js
+++ 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.js
@@ -1,10 +1,13 @@
 const solution = (a, b) => {
   let n = 1;
-  for (let i = 1; i <= Math.min(a, b); i++) {
-    if (a % i === 0 && b % i === 0) n = i;
+  for (let i = 2; i <= Math.min(a, b); i++) {
+    if (!(a % i) && !(b % i)) n = i;
   }
   b /= n;
-  while (b % 2 === 0) b /= 2;
-  while (b % 5 === 0) b /= 5;
-  return b === 1 ? 1 : 2;
+  for (let j = 2; j <= b; j++) {
+    if (j % 2 && j % 5 && !(b % j)) {
+      return 2;
+    }
+  }
+  return 1;
 };
