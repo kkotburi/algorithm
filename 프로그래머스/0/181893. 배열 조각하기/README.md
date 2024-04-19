f9114d109b90aad5a300764c97a336b41501ea89
kkotburi <128236313+kkotburi@users.noreply.github.com>
2024-04-19 23:45:25 +0900

    [level 0] Title: 배열 조각하기, Time: 0.21 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/0/181893. 배열 조각하기/README.md
+++ 프로그래머스/0/181893. 배열 조각하기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 배열 조각하기 - 181893 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181893) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181893?language=javascript) 
 
 ### 성능 요약
 
-메모리: 33.7 MB, 시간: 0.31 ms
+메모리: 33.7 MB, 시간: 0.21 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 19일 23:39:01
+2024년 04월 19일 23:45:24
 
 ### 문제 설명
 
--- 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.js
+++ 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.js
@@ -1,11 +1,8 @@
-const solution = (arr, query) => {
-  let answer = arr;
-  for (let i = 0; i < query.length; i++) {
-    if (i % 2) {
-      answer = answer.slice(query[i]);
-    } else {
-      answer = answer.slice(0, query[i] + 1);
-    }
-  }
-  return answer;
-};
+const solution = (arr, query, n = 0) =>
+  n < query.length
+    ? solution(
+        n % 2 ? arr.slice(query[n]) : arr.slice(0, query[n] + 1),
+        query,
+        n + 1
+      )
+    : arr;
