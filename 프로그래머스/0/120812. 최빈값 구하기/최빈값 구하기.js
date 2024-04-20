8b34d28431aa37187c865ae3e5aed380b9099cd0
kkotburi <128236313+kkotburi@users.noreply.github.com>
2024-04-21 00:15:28 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.22 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.js
+++ 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.js
@@ -3,12 +3,10 @@ function solution(array) {
   let answer = 0;
   for (let i = 0; i < array.length; i++) {
     let length = array.filter((v) => v === array[i]).length;
-    if (array[i] === answer) {
-      i++;
-    } else if (length > frequency) {
+    if (array[i] !== answer && length > frequency) {
       frequency = length;
       answer = array[i];
-    } else if (length === frequency) {
+    } else if (array[i] !== answer && length === frequency) {
       answer = -1;
     }
   }
