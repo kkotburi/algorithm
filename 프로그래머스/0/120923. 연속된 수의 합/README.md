9bd1f18780566083035ca610eda5eba6bec824fa
kkotburi <128236313+kkotburi@users.noreply.github.com>
2024-04-24 01:20:23 +0900

    [level 0] Title: 연속된 수의 합, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120923. 연속된 수의 합/README.md
+++ 프로그래머스/0/120923. 연속된 수의 합/README.md
@@ -1,10 +1,10 @@
 # [level 0] 연속된 수의 합 - 120923 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120923) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120923?language=javascript) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.18 ms
+메모리: 33.5 MB, 시간: 0.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 23일 23:55:25
+2024년 04월 24일 01:20:23
 
 ### 문제 설명
 
--- 프로그래머스/0/120923. 연속된 수의 합/연속된 수의 합.js
+++ 프로그래머스/0/120923. 연속된 수의 합/연속된 수의 합.js
@@ -1,13 +1,6 @@
 const solution = (num, total) => {
-  let answer = [];
-  if (total % num) {
-    answer = [Math.floor(total / num), Math.ceil(total / num)];
-  } else {
-    answer = [total / num];
-  }
-  for (let i = 1; i < num / 2; i++) {
-    answer.push(Math.floor(total / num) - i);
-    answer.push(Math.ceil(total / num) + i);
-  }
-  return answer.sort((a, b) => a - b);
+  const min = Math.ceil(total / num - num / 2);
+    return Array(num)
+    .fill()
+    .map((_, i) => i + min);
 };
