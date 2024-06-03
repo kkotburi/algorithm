97e033d6a997787311797f57f1b2f85f6ca6d959
kkotburi <128236313+kkotburi@users.noreply.github.com>
2024-06-03 23:31:15 +0900

    [level 1] Title: K번째수, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.js
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -1,11 +1,8 @@
-const solution = (array, commands) => {
-  let answer = [];
-  for (let i = 0; i < commands.length; i++) {
-    let arraySlice = [];
-    for (let j = commands[i][0] - 1; j < commands[i][1]; j++) {
-      arraySlice.push(array[j]);
-    }
-    answer.push(arraySlice.sort((a, b) => a - b)[commands[i][2] - 1]);
-  }
-  return answer;
-};
+const solution = (array, commands) =>
+  commands.map(
+    (v) =>
+      array
+        .slice(v[0] - 1, v[1])
+        .sort((a, b) => a - b)
+        .slice(v[2] - 1, v[2])[0]
+  );
