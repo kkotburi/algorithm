325b8210b0d219e5d680f14296b8592eabcf8896
kkotburi <128236313+kkotburi@users.noreply.github.com>
2024-06-05 23:57:08 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.js
@@ -0,0 +1,9 @@
+const solution = (food) => {
+  let answer = "";
+  for (let i = 1; i < food.length; i++) {
+    for (let j = 1; j <= Math.floor(food[i] / 2); j++) {
+      answer += `${i}`;
+    }
+  }
+  return answer + "0" + answer.split("").reverse().join("");
+};
