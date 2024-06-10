b65c6fd6403253b10595e55dbb517262f99066c0
kkotburi <128236313+kkotburi@users.noreply.github.com>
2024-06-10 23:59:26 +0900

    [level 1] Title: 카드 뭉치, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
@@ -0,0 +1,12 @@
+const solution = (cards1, cards2, goal) => {
+  for (let i of goal) {
+    if (cards1[0] === i) {
+      cards1.shift();
+    } else if (cards2[0] === i) {
+      cards2.shift();
+    } else {
+      return "No";
+    }
+  }
+  return "Yes";
+};
