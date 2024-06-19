4069c63d0e6682212dc9e73914596dacfe8a5880
kkotburi <128236313+kkotburi@users.noreply.github.com>
2024-06-19 22:12:57 +0900

    [level 1] Title: 추억 점수, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/추억 점수.js
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.js
@@ -3,10 +3,8 @@ const solution = (name, yearning, photo) => {
   for (let i = 0; i < photo.length; i++) {
     let sum = 0;
     for (let j = 0; j < photo[i].length; j++) {
-      for (let k = 0; k < name.length; k++) {
-        if (name[k] === photo[i][j]) {
-          sum += yearning[k];
-        }
+      if (name.indexOf(photo[i][j]) !== -1) {
+        sum += yearning[name.indexOf(photo[i][j])];
       }
     }
     answer.push(sum);
