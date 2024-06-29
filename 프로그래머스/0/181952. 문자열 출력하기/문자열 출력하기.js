ea118cf8bcc67669ce6558589ef3619f7f730477
kkotburi <128236313+kkotburi@users.noreply.github.com>
2024-06-29 23:49:03 +0900

    [level 0] Title: 문자열 출력하기, Time: 42.21 ms, Memory: 32.2 MB -BaekjoonHub

--- 프로그래머스/0/181952. 문자열 출력하기/문자열 출력하기.js
+++ 프로그래머스/0/181952. 문자열 출력하기/문자열 출력하기.js
@@ -1,14 +1,7 @@
 const readline = require("readline");
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-let input = [];
-
-rl.on("line", function (line) {
-  input = [line];
-}).on("close", function () {
-  str = input[0];
-  console.log(str);
-});
+const rl = readline
+  .createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  })
+  .on("line", console.log);
