8ed916312a6178fcc0ca92e39f5640e7386a8fc3
kkotburi <128236313+kkotburi@users.noreply.github.com>
2024-07-02 23:55:33 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 8.01 ms, Memory: 36.5 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
@@ -1,5 +1,16 @@
 const solution = (n, m) => {
-  let r;
-  for (var nm = n * m; (r = n % m); n = m, m = r) {}
-  return [m, nm / m];
+  let answer = [];
+  for (let i = Math.min(n, m); i > 0; i--) {
+    if (!(n % i) && !(m % i)) {
+      answer.push(i);
+      break;
+    }
+  }
+  for (let j = Math.max(n, m); j >= Math.max(n, m); j++) {
+    if (!(j % n) && !(j % m)) {
+      answer.push(j);
+      break;
+    }
+  }
+  return answer;
 };
