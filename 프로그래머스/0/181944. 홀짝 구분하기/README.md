4e1b0a512ca3130ad06a48d5b1706d592897fd5d
kkotburi <128236313+kkotburi@users.noreply.github.com>
2024-07-03 23:40:42 +0900

    [level 0] Title: 홀짝 구분하기, Time: 42.49 ms, Memory: 32.2 MB -BaekjoonHub

--- 프로그래머스/0/181944. 홀짝 구분하기/README.md
+++ 프로그래머스/0/181944. 홀짝 구분하기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 홀짝 구분하기 - 181944 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181944) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181944?language=javascript) 
 
 ### 성능 요약
 
-메모리: 32.3 MB, 시간: 42.79 ms
+메모리: 32.2 MB, 시간: 42.49 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 03일 23:29:37
+2024년 07월 03일 23:40:40
 
 ### 문제 설명
 
--- 프로그래머스/0/181944. 홀짝 구분하기/홀짝 구분하기.js
+++ 프로그래머스/0/181944. 홀짝 구분하기/홀짝 구분하기.js
@@ -1,18 +1,10 @@
 const readline = require("readline");
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-let input = [];
-
-rl.on("line", function (line) {
-  input = line.split(" ");
-}).on("close", function () {
-  n = Number(input[0]);
-  if (n % 2) {
-    console.log(n, "is odd");
-  } else {
-    console.log(n, "is even");
-  }
-});
+const rl = readline
+  .createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  })
+  .on("line", function (n) {
+    let isOdd = n % 2 ? "odd" : "even";
+    console.log(`${n} is ${isOdd}`);
+  });
