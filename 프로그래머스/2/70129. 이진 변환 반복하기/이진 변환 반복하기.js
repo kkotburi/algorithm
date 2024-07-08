8a3e7a8d12f5339808d7832ffaacfca85a6512bd
kkotburi <128236313+kkotburi@users.noreply.github.com>
2024-07-08 23:33:50 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 5.57 ms, Memory: 35.3 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
@@ -1,9 +1,9 @@
 const solution = (s) => {
   let answer = [0, 0];
-  while (s.length > 1) {
+  while (s !== "1") {
     answer[0]++;
-    answer[1] += (s.match(/0/g) || []).length;
-    s = s.replace(/0/g, "").length.toString(2);
+    answer[1] += s.split("0").length - 1;
+    s = s.split("0").join("").length.toString(2);
   }
   return answer;
 };
