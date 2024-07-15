521707d62e3371577704d1adc8991a9a9e9b05ba
kkotburi <128236313+kkotburi@users.noreply.github.com>
2024-07-16 02:06:21 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.22 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.js
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.js
@@ -1,19 +1,14 @@
-function solution(n, words) {
-  let answer = [0, 0];
-
+const solution = (n, words) => {
   for (let i = 0; i < words.length; i++) {
     let word = words[i];
-    let p = (i % n) + 1;
+    let person = (i % n) + 1;
     let turn = Math.ceil((i + 1) / n);
-
     if (i > 0) {
       let last = words[i - 1].split("").pop();
       if (i > words.indexOf(word) || words[i][0] !== last) {
-        answer = [p, turn];
-        break;
+        return [person, turn];
       }
     }
   }
-
-  return answer;
-}
+  return [0, 0];
+};
