3e6a9c15ed6f042d52bd879c1dfc475ca98ada80
kkotburi <128236313+kkotburi@users.noreply.github.com>
2024-07-19 20:51:50 +0900

    [level 0] Title: 안전지대, Time: 0.23 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120866. 안전지대/안전지대.js
+++ 프로그래머스/0/120866. 안전지대/안전지대.js
@@ -1,23 +1,21 @@
 const solution = (board) => {
   let answer = 0;
-  let danger = [
-    [-1, 0],
-    [-1, -1],
-    [-1, 1],
-    [0, -1],
-    [0, 1],
-    [1, 0],
-    [1, -1],
-    [1, 1],
-  ];
-  board.forEach((row, column, array) =>
-    row.forEach((element, index) => {
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board[i].length; j++) {
       if (
-        element !== 1 &&
-        !danger.some(([x, y]) => !!array[column + y]?.[index + x])
-      )
+        !board[i - 1]?.[j - 1] &&
+        !board[i - 1]?.[j] &&
+        !board[i - 1]?.[j + 1] &&
+        !board[i][j - 1] &&
+        !board[i][j] &&
+        !board[i][j + 1] &&
+        !board[i + 1]?.[j - 1] &&
+        !board[i + 1]?.[j] &&
+        !board[i + 1]?.[j + 1]
+      ) {
         answer++;
-    })
-  );
+      }
+    }
+  }
   return answer;
 };
