78fa2832a63b7fd61e4eea8d7d49fcc0913f4bea
kkotburi <128236313+kkotburi@users.noreply.github.com>
2024-07-21 23:51:49 +0900

    [level 0] Title: 평행, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120875. 평행/README.md
+++ 프로그래머스/0/120875. 평행/README.md
@@ -1,6 +1,6 @@
 # [level 0] 평행 - 120875 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120875) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120875?language=javascript) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 28일 23:56:05
+2024년 07월 21일 23:51:47
 
 ### 문제 설명
 
--- 프로그래머스/0/120875. 평행/평행.js
+++ 프로그래머스/0/120875. 평행/평행.js
@@ -1,20 +1,10 @@
-function solution(dots) {
-  var answer = 0;
+const slope = (arr1, arr2, arr3, arr4) =>
+  (arr2[1] - arr1[1]) / (arr2[0] - arr1[0]) ===
+  (arr4[1] - arr3[1]) / (arr4[0] - arr3[0]);
 
-  function calculation(a, b, c, d) {
-    let abDiff, cdDiff;
-
-    abDiff = (b[1] - a[1]) / (b[0] - a[0]);
-    cdDiff = (d[1] - c[1]) / (d[0] - c[0]);
-
-    if (abDiff === cdDiff) {
-      answer += 1;
-    }
-  }
-
-  calculation(dots[0], dots[1], dots[2], dots[3]);
-  calculation(dots[0], dots[2], dots[1], dots[3]);
-  calculation(dots[0], dots[3], dots[1], dots[2]);
-
-  return answer > 0 ? 1 : 0;
-}
+const solution = (dots) =>
+  slope(dots[0], dots[1], dots[2], dots[3]) ||
+  slope(dots[0], dots[2], dots[1], dots[3]) ||
+  slope(dots[0], dots[3], dots[1], dots[2])
+    ? 1
+    : 0;
