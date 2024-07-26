e5e5c2545bdaf818bd938a22c4626403ce7c8616
kkotburi <128236313+kkotburi@users.noreply.github.com>
2024-07-26 22:09:00 +0900

    [level 2] Title: 의상, Time: 0.20 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.js
+++ 프로그래머스/2/42578. 의상/의상.js
@@ -1,11 +1,7 @@
-const solution = (clothes) => {
-  let answer = 1;
-  let type = [];
-  for (let i = 0; i < clothes.length; i++) {
-    if (!type.find((v) => v === clothes[i][1])) {
-      type.push(clothes[i][1]);
-      answer *= clothes.filter((v) => v[1] === clothes[i][1]).length + 1;
-    }
-  }
-  return answer - 1;
-};
+const solution = (clothes) =>
+  Object.values(
+    clothes.reduce((acc, obj) => {
+      acc[obj[1]] = acc[obj[1]] ? acc[obj[1]] + 1 : 1;
+      return acc;
+    }, {})
+  ).reduce((a, b) => a * (b + 1), 1) - 1;
