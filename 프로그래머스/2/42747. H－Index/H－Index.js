9bd77fc3b274d70e1555412191c8705a73a35ef7
kkotburi <128236313+kkotburi@users.noreply.github.com>
2024-07-31 20:08:47 +0900

    [level 2] Title: H-Index, Time: 0.36 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.js
+++ 프로그래머스/2/42747. H－Index/H－Index.js
@@ -1,6 +1,2 @@
-const solution = (citations) => {
-  citations.sort((a, b) => b - a);
-  let answer = 0;
-  while (citations[answer] > answer) answer++;
-  return answer;
-};
+const solution = (citations) =>
+  citations.sort((a, b) => b - a).filter((v, i) => v >= i + 1).length;
