41c8be0443b29bf41db89e3e0f021f61b5c968c8
kkotburi <128236313+kkotburi@users.noreply.github.com>
2024-08-02 23:49:18 +0900

    [level 0] Title: 주사위 게임 3, Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.js
+++ 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.js
@@ -1,21 +1,28 @@
 const solution = (a, b, c, d) => {
-  if (a === b && a === c && a === d) return 1111 * a;
+  let arr = [a, b, c, d];
+  let obj = { 0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0 };
 
-  if (a === b && a === c) return (10 * a + d) ** 2;
-  if (a === b && a === d) return (10 * a + c) ** 2;
-  if (a === c && a === d) return (10 * a + b) ** 2;
-  if (b === c && b === d) return (10 * b + a) ** 2;
+  for (let i = 0; i < arr.length; i++) {
+    obj[arr[i]]++;
+  }
 
-  if (a === b && c === d) return (a + c) * Math.abs(a - c);
-  if (a === c && b === d) return (a + b) * Math.abs(a - b);
-  if (a === d && b === c) return (a + b) * Math.abs(a - b);
+  let values = Object.values(obj);
 
-  if (a === b) return c * d;
-  if (a === c) return b * d;
-  if (a === d) return b * c;
-  if (b === c) return a * d;
-  if (b === d) return a * c;
-  if (c === d) return a * b;
+  if (values.find((v) => v === 4)) {
+    return 1111 * values.indexOf(4);
+  }
+  if (values.find((v) => v === 3)) {
+    return (10 * values.indexOf(3) + values.indexOf(1)) ** 2;
+  }
+  if (values.filter((v) => v === 2).length === 2) {
+    return (
+      (values.indexOf(2) + values.lastIndexOf(2)) *
+      Math.abs(values.indexOf(2) - values.lastIndexOf(2))
+    );
+  }
+  if (values.filter((v) => v === 2).length === 1) {
+    return values.indexOf(1) * values.lastIndexOf(1);
+  }
 
-  return Math.min(a, b, c, d);
+  return Math.min(...arr);
 };
