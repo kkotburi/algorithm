175a646322fb6f93643ec3e2d442601fe805add8
kkotburi <128236313+kkotburi@users.noreply.github.com>
2024-08-03 00:31:39 +0900

    [level 0] Title: 주사위 게임 3, Time: 0.12 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.js
+++ 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.js
@@ -1,22 +1,20 @@
 const solution = (a, b, c, d) => {
-  let arr = [a, b, c, d];
-  
   let map = new Map();
-  for (const num of arr) {
+  for (const num of [a, b, c, d]) {
     map.set(num, (map.get(num) || 0) + 1);
   }
+  let num = [...map.keys()].sort((a, b) => map.get(b) - map.get(a));
+  let [p, q, r, s] = num;
 
-  let sortedMap = [...map.keys()].sort((a, b) => map.get(b) - map.get(a));
-  let maxFqc = Math.max(...map.values());
-  let [p, q, r, s] = sortedMap;
-
-  if (sortedMap.length === 1) {
+  if (num.length === 1) {
     return p * 1111;
   }
-  if (sortedMap.length === 2) {
-    return maxFqc === 3 ? (10 * p + q) ** 2 : (p + q) * Math.abs(p - q);
+  if (num.length === 2) {
+    return Math.max(...map.values()) === 3
+      ? (10 * p + q) ** 2
+      : (p + q) * Math.abs(p - q);
   }
-  if (sortedMap.length === 3) {
+  if (num.length === 3) {
     return q * r;
   }
 
