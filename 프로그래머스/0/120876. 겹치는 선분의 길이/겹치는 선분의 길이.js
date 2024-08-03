ceb851307068a42f393261d074d23224af1183c1
kkotburi <128236313+kkotburi@users.noreply.github.com>
2024-08-03 23:56:12 +0900

    [level 0] Title: 겹치는 선분의 길이, Time: 0.26 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.js
+++ 프로그래머스/0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.js
@@ -1,10 +1,13 @@
 const solution = (lines) => {
-  let line = new Array(200).fill(0);
-  lines.forEach(([a, b]) => {
-    while (a < b) {
-      line[a + 100]++;
-      a++;
+  let start = Math.min(...lines.flat());
+  let end = Math.max(...lines.flat());
+  let lst = [...Array(end - start)].fill(0);
+
+  lines.forEach((el) => {
+    for (let i = el[0]; i < el[1]; i++) {
+      lst[i - start] += 1;
     }
   });
-  return line.reduce((acc, curr) => (curr > 1 ? acc + 1 : acc), 0);
+
+  return lst.reduce((a, c) => (c > 1 ? a + 1 : a), 0);
 };
