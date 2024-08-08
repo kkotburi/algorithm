b305988b3f43f78a21061535630ef5a4a0ee9159
kkotburi <128236313+kkotburi@users.noreply.github.com>
2024-08-08 18:21:29 +0900

    [level 1] Title: 모의고사, Time: 2.71 ms, Memory: 35.6 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.js
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -3,13 +3,9 @@ const solution = (answers) => {
   let a2 = [2, 1, 2, 3, 2, 4, 2, 5];
   let a3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
 
-  let b1 = answers.filter((v, i) => v === a1[i % a1.length]).length;
-  let b2 = answers.filter((v, i) => v === a2[i % a2.length]).length;
-  let b3 = answers.filter((v, i) => v === a3[i % a3.length]).length;
+  let score = [a1, a2, a3].map(
+    (a) => answers.filter((v, i) => v === a[i % a.length]).length
+  );
 
-  let answer = [b1, b2, b3];
-
-  return answer
-    .map((_, i) => i + 1)
-    .filter((_, i) => answer[i] === Math.max(...answer));
+  return [1, 2, 3].filter((_, i) => score[i] === Math.max(...score));
 };
