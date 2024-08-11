52c225844136b104640f60fdc978b7bb8afd23f3
kkotburi <128236313+kkotburi@users.noreply.github.com>
2024-08-11 23:14:13 +0900

    [level 0] Title: 정수를 나선형으로 배치하기, Time: 0.35 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.js
+++ 프로그래머스/0/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.js
@@ -1,33 +1,27 @@
 const solution = (n) => {
+  let answer = Array.from(new Array(n), () => new Array(n).fill(0));
   let move = [
     [0, 1],
     [1, 0],
     [0, -1],
     [-1, 0],
   ];
-  let answer = Array.from(new Array(n), () => new Array(n).fill(0));
-  let x = 0;
-  let y = 0;
+  let col = 0;
+  let row = 0;
   let dir = 0;
   let num = 1;
+
   while (num <= n * n) {
-    answer[x][y] = num;
-    let nextX = x + move[dir][0];
-    let nextY = y + move[dir][1];
-    if (
-      nextX >= n ||
-      nextX < 0 ||
-      nextY >= n ||
-      nextY < 0 ||
-      answer[nextX][nextY] !== 0
-    ) {
+    answer[col][row] = num;
+    let x = col + move[dir][0];
+    let y = row + move[dir][1];
+    if (x >= n || x < 0 || y >= n || y < 0 || answer[x][y]) {
       dir = (dir + 1) % 4;
-      nextX = x + move[dir][0];
-      nextY = y + move[dir][1];
     }
-    x = nextX;
-    y = nextY;
+    col += move[dir][0];
+    row += move[dir][1];
     num++;
   }
+
   return answer;
 };
