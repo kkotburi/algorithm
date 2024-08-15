c2933d5d1c4dc6e3ea8b826a3f8c76efed75a716
kkotburi <128236313+kkotburi@users.noreply.github.com>
2024-08-15 12:50:04 +0900

    [level 2] Title: 할인 행사, Time: 101.25 ms, Memory: 40.1 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.js
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.js
@@ -1,14 +1,11 @@
+const equals = (a, b) => JSON.stringify(a) === JSON.stringify(b);
 const solution = (want, number, discount) => {
   let answer = 0;
-  for (let i = 0; i < discount.length - 9; i++) {
-    let slice = discount.slice(i, i + 10);
-    for (let j = 0; j < want.length; j++) {
-      if (slice.filter((item) => item === want[j]).length !== number[j]) {
-        break;
-      } else if (j === want.length - 1) {
-        answer++;
-      }
-    }
+  let array = new Array(number.length).fill(0);
+  for (let i = 0; i < discount.length; i++) {
+    array[want.indexOf(discount[i])]++;
+    array[want.indexOf(discount[i - 10])]--;
+    if (equals(array, number)) answer++;
   }
   return answer;
 };
