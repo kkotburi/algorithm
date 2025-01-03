9d2a0d9dac38cc9242a8417aa1502a306ec1a45e
Hyeyeong Lee <128236313+kkotburi@users.noreply.github.com>
2025-01-03 23:53:22 +0900

    [level 2] Title: 모음 사전, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.js
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.js
@@ -1,8 +1,5 @@
 const solution = (words) =>
-  words
-    .split("")
-    .reduce(
-      (r, c, i) =>
-        r + [781, 156, 31, 6, 1][i] * ["A", "E", "I", "O", "U"].indexOf(c) + 1,
-      0
-    );
+  [...words].reduce(
+    (a, c, i) => a + "AEIOU".indexOf(c) * ~~(781 / 5 ** i) + 1,
+    0
+  );
