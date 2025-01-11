d71c88c684eb1caab8ac63a5c77124104e56a7f1
Hyeyeong Lee <128236313+kkotburi@users.noreply.github.com>
2025-01-11 23:37:46 +0900

    [level 3] Title: 야근 지수, Time: 141.81 ms, Memory: 37.4 MB -BaekjoonHub

--- 프로그래머스/3/12927. 야근 지수/README.md
+++ 프로그래머스/3/12927. 야근 지수/README.md
@@ -1,10 +1,10 @@
 # [level 3] 야근 지수 - 12927 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12927) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12927?language=javascript) 
 
 ### 성능 요약
 
-메모리: 36.2 MB, 시간: 46.39 ms
+메모리: 37.4 MB, 시간: 141.81 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 10일 23:54:57
+2025년 01월 11일 23:37:44
 
 ### 문제 설명
 
--- 프로그래머스/3/12927. 야근 지수/야근 지수.js
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.js
@@ -1,15 +1,68 @@
-const solution = (n, works) => {
-  if (works.reduce((acc, cur) => acc + cur, 0) <= n) return 0;
-  let sorted = works.sort((a, b) => b - a);
-  while (n) {
-    let max = sorted[0];
-    for (let i = 0; i < sorted.length; i++) {
-      if (sorted[i] >= max) {
-        sorted[i]--;
-        n--;
-      }
-      if (!n) break;
+class MaxHeap {
+  constructor() {
+    this.heap = [null];
+  }
+
+  push(value) {
+    this.heap.push(value);
+    let currentIndex = this.heap.length - 1;
+    let parentIndex = Math.floor(currentIndex / 2);
+
+    while (parentIndex !== 0 && this.heap[parentIndex] < value) {
+      const temp = this.heap[parentIndex];
+      this.heap[parentIndex] = value;
+      this.heap[currentIndex] = temp;
+
+      currentIndex = parentIndex;
+      parentIndex = Math.floor(currentIndex / 2);
     }
   }
-  return works.reduce((acc, cur) => acc + cur ** 2, 0);
+
+  pop() {
+    if (this.heap.length === 2) return this.heap.pop();
+
+    const returnValue = this.heap[1];
+    this.heap[1] = this.heap.pop();
+
+    let currentIndex = 1;
+    let leftIndex = 2;
+    let rightIndex = 3;
+    while (
+      this.heap[currentIndex] < this.heap[leftIndex] ||
+      this.heap[currentIndex] < this.heap[rightIndex]
+    ) {
+      if (this.heap[leftIndex] < this.heap[rightIndex]) {
+        const temp = this.heap[currentIndex];
+        this.heap[currentIndex] = this.heap[rightIndex];
+        this.heap[rightIndex] = temp;
+        currentIndex = rightIndex;
+      } else {
+        const temp = this.heap[currentIndex];
+        this.heap[currentIndex] = this.heap[leftIndex];
+        this.heap[leftIndex] = temp;
+        currentIndex = leftIndex;
+      }
+      leftIndex = currentIndex * 2;
+      rightIndex = currentIndex * 2 + 1;
+    }
+    return returnValue;
+  }
+}
+
+const solution = (n, works) => {
+  const heap = new MaxHeap();
+
+  for (let i = 0; i < works.length; i++) {
+    heap.push(works[i]);
+  }
+
+  for (let i = 0; i < n; i++) {
+    let peekValue = heap.pop() - 1;
+    if (peekValue < 0) peekValue = 0;
+    heap.push(peekValue);
+  }
+
+  return heap.heap
+    .filter((item) => item !== null)
+    .reduce((acc, item) => acc + item ** 2, 0);
 };
