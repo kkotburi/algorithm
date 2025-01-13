2904eb5af1b8a73513db8a8f1cc547b8892f9005
Hyeyeong Lee <128236313+kkotburi@users.noreply.github.com>
2025-01-13 23:57:35 +0900

    [level 2] Title: 뒤에 있는 큰 수 찾기, Time: 449.25 ms, Memory: 126 MB -BaekjoonHub

---
+++ 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.js
@@ -0,0 +1,11 @@
+const solution = (numbers) => {
+  let answer = new Array(numbers.length).fill(-1);
+  let stack = [];
+  for (var i = 0; i < numbers.length; i++) {
+    while (stack && numbers[stack.at(-1)] < numbers[i]) {
+      answer[stack.pop()] = numbers[i];
+    }
+    stack.push(i);
+  }
+  return answer;
+};
