e360a1b4a62402e7596bad64aeba5cb4a4855319
Hyeyeong Lee <128236313+kkotburi@users.noreply.github.com>
2025-01-16 23:31:26 +0900

    [level 2] Title: 땅따먹기, Time: 47.41 ms, Memory: 72.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.js
@@ -0,0 +1,12 @@
+const solution = (land) =>
+  Math.max(
+    ...land.reduce(
+      (a, c) => [
+        c[0] + Math.max(a[1], a[2], a[3]),
+        c[1] + Math.max(a[0], a[2], a[3]),
+        c[2] + Math.max(a[0], a[1], a[3]),
+        c[3] + Math.max(a[0], a[1], a[2]),
+      ],
+      [0, 0, 0, 0]
+    )
+  );
