82626d2b0640f88e735ac290ce2d855ee967b3e7
Hyeyeong Lee <128236313+kkotburi@users.noreply.github.com>
2025-05-14 00:46:54 +0900

    [level 2] Title: 더 맵게, Time: 484.30 ms, Memory: 112 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/README.md
+++ 프로그래머스/2/42626. 더 맵게/README.md
@@ -1,10 +1,10 @@
 # [level 2] 더 맵게 - 42626 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42626) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42626?language=javascript) 
 
 ### 성능 요약
 
-메모리: 101 MB, 시간: 399.31 ms
+메모리: 112 MB, 시간: 484.30 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 13일 23:25:17
+2025년 05월 14일 00:46:53
 
 ### 문제 설명
 
--- 프로그래머스/2/42626. 더 맵게/더 맵게.js
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.js
@@ -1,74 +1,75 @@
-class MinHeap {
-  constructor() {
-    this.heap = [];
-  }
-
-  size() {
-    return this.heap.length;
-  }
-
-  push(value) {
-    this.heap.push(value);
-    let currentIndex = this.heap.length - 1;
-
-    while (
-      currentIndex > 0 &&
-      this.heap[currentIndex] < this.heap[Math.floor((currentIndex - 1) / 2)]
-    ) {
-      const temp = this.heap[currentIndex];
-      this.heap[currentIndex] = this.heap[Math.floor((currentIndex - 1) / 2)];
-      this.heap[Math.floor((currentIndex - 1) / 2)] = temp;
-      currentIndex = Math.floor((currentIndex - 1) / 2);
-    }
-  }
-
-  pop() {
-    if (this.heap.length === 0) return null;
-    if (this.heap.length === 1) return this.heap.pop();
-
-    const minValue = this.heap[0];
-    this.heap[0] = this.heap.pop();
-    let currentIndex = 0;
-
-    while (currentIndex * 2 + 1 < this.heap.length) {
-      let minChildIndex =
-        currentIndex * 2 + 2 < this.heap.length &&
-        this.heap[currentIndex * 2 + 2] < this.heap[currentIndex * 2 + 1]
-          ? currentIndex * 2 + 2
-          : currentIndex * 2 + 1;
-
-      if (this.heap[currentIndex] < this.heap[minChildIndex]) break;
-
-      const temp = this.heap[currentIndex];
-      this.heap[currentIndex] = this.heap[minChildIndex];
-      this.heap[minChildIndex] = temp;
-      currentIndex = minChildIndex;
-    }
-
-    return minValue;
-  }
-
-  peek() {
-    return this.heap[0];
-  }
-}
-
 const solution = (scoville, K) => {
-  const minHeap = new MinHeap();
+  scoville.sort((a, b) => b - a);
+  let cnt = 0;
+  let under_K = [];
+  let flag = 0;
 
-  for (const sco of scoville) {
-    minHeap.push(sco);
+  for (let i = 0; i < scoville.length; ++i) {
+    if (scoville[i] < K) {
+      under_K.push(scoville[i]);
+    } else {
+      flag = 1;
+    }
   }
 
-  let mixedCount = 0;
+  let mixed = [];
+  let m_i = 0;
+  let i = 0;
+  let n1 = 0;
+  let n2 = 0;
 
-  while (minHeap.size() >= 2 && minHeap.peek() < K) {
-    const first = minHeap.pop();
-    const second = minHeap.pop();
-    const mixedScov = first + second * 2;
-    minHeap.push(mixedScov);
-    mixedCount++;
+  while (1) {
+    if (mixed[m_i] != undefined) {
+      if (under_K.length != 0) {
+        if (under_K.at(-1) < mixed[m_i]) {
+          n1 = under_K.pop();
+        } else {
+          n1 = mixed[m_i];
+          ++m_i;
+        }
+      } else {
+        n1 = mixed[m_i];
+        ++m_i;
+      }
+    } else {
+      if (under_K.length != 0) {
+        n1 = under_K.pop();
+      } else {
+        break;
+      }
+    }
+    if (mixed[m_i] != undefined) {
+      if (under_K.length != 0) {
+        if (under_K.at(-1) < mixed[m_i]) {
+          n2 = under_K.pop();
+        } else {
+          n2 = mixed[m_i];
+          ++m_i;
+        }
+      } else {
+        n2 = mixed[m_i];
+        ++m_i;
+      }
+    } else {
+      if (under_K.length != 0) {
+        n2 = under_K.pop();
+      } else {
+        ++cnt;
+        break;
+      }
+    }
+
+    if (n1 + n2 * 2 < K) {
+      mixed.push(n1 + n2 * 2);
+    } else {
+      flag = 1;
+    }
+    ++cnt;
   }
 
-  return minHeap.peek() >= K ? mixedCount : -1;
+  if (flag == 1) {
+    return cnt;
+  } else {
+    return -1;
+  }
 };
