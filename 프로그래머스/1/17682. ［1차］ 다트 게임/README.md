a1d98441ce1493efd07392ee51aa234f82730b6e
Hyeyeong Lee <128236313+kkotburi@users.noreply.github.com>
2025-05-22 23:45:03 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.34 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/17682. ［1차］ 다트 게임/README.md
+++ 프로그래머스/1/17682. ［1차］ 다트 게임/README.md
@@ -1,10 +1,10 @@
 # [level 1] [1차] 다트 게임 - 17682 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17682) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17682?language=javascript) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.23 ms
+메모리: 33.5 MB, 시간: 0.34 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 21일 23:58:01
+2025년 05월 22일 23:45:02
 
 ### 문제 설명
 
--- 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.js
+++ 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.js
@@ -1,32 +1,16 @@
 const solution = (dartResult) => {
-  let dart = dartResult.split("");
-  let score = [];
+  const bonus = { S: 1, D: 2, T: 3 };
+  const options = { "*": 2, "#": -1, undefined: 1 };
+  let darts = dartResult.match(/\d.?\D/g);
 
-  for (let i = 0; i < dart.length; i++) {
-    if (dart[i] === "0" && dart[i - 1] === "1") dart[i] = 10;
-    if (dart[i] === "S") score.push(Math.pow(dart[i - 1], 1));
-    if (dart[i] === "D") score.push(Math.pow(dart[i - 1], 2));
-    if (dart[i] === "T") score.push(Math.pow(dart[i - 1], 3));
-    if (dart[i] === "*") score.push("*");
-    if (dart[i] === "#") score.push("#");
+  for (let i = 0; i < darts.length; i++) {
+    let split = darts[i].match(/(^\d{1,})(S|D|T)(\*|#)?/);
+    let score = Math.pow(split[1], bonus[split[2]]) * options[split[3]];
+
+    if (split[3] === "*" && darts[i - 1]) darts[i - 1] *= options["*"];
+
+    darts[i] = score;
   }
 
-  for (let i = 0; i < score.length; i++) {
-    if (score[i] === "#") {
-      score[i - 1] = score[i - 1] * -1;
-    }
-    if (score[i] === "*") {
-      if (isNaN(score[i - 2])) {
-        score = score.map((val, index) =>
-          index < i && index >= i - 3 && !isNaN(val) ? val * 2 : val
-        );
-      } else {
-        score = score.map((val, index) =>
-          index < i && index >= i - 2 && !isNaN(val) ? val * 2 : val
-        );
-      }
-    }
-  }
-
-  return score.filter((val) => !isNaN(val)).reduce((acc, cur) => acc + cur, 0);
+  return darts.reduce((a, b) => a + b);
 };
