674fa914ca09e096481671cb8bc7534b83f711d6
Hyeyeong Lee <128236313+kkotburi@users.noreply.github.com>
2025-05-29 23:56:46 +0900

    [level 2] Title: 연속된 부분 수열의 합, Time: 245.57 ms, Memory: 168 MB -BaekjoonHub

---
+++ 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.js
@@ -0,0 +1,19 @@
+const solution = (sequence, k) => {
+  let [start, end] = [0, 0];
+  let sum = sequence[start];
+  const answer = [];
+
+  while (end < sequence.length) {
+    if (sum < k) {
+      sum += sequence[++end];
+    } else if (sum > k) {
+      sum -= sequence[start++];
+    } else {
+      answer.push([start, end]);
+      sum += sequence[++end];
+      sum -= sequence[start++];
+    }
+  }
+
+  return answer.sort((a, b) => a[1] - a[0] - (b[1] - b[0]))[0];
+};
