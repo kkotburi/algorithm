1a72e30defc410e6e0d8b370f6946ac7749a4de2
Hyeyeong Lee <128236313+kkotburi@users.noreply.github.com>
2025-06-03 21:49:00 +0900

    [level 2] Title: 방문 길이, Time: 0.78 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.js
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.js
@@ -1,11 +1,11 @@
 const solution = (dirs) => {
-  let answer = new Set();
-  let move = { L: [-1, 0], R: [1, 0], U: [0, 1], D: [0, -1] };
+  const direction = { L: [-1, 0], R: [1, 0], U: [0, 1], D: [0, -1] };
   let current = [0, 0];
+  let answer = new Set();
 
   for (let dir of dirs) {
-    let currentX = current[0] + move[dir][0];
-    let currentY = current[1] + move[dir][1];
+    let currentX = current[0] + direction[dir][0];
+    let currentY = current[1] + direction[dir][1];
 
     if (currentX > 5 || currentX < -5 || currentY > 5 || currentY < -5)
       continue;
