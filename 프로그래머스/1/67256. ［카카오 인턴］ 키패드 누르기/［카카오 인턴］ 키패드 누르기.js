f4303338b5e1dedd5e9bc1679a989400301ce125
Hyeyeong Lee <128236313+kkotburi@users.noreply.github.com>
2025-06-04 23:58:40 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 0.68 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.js
@@ -0,0 +1,54 @@
+const solution = (numbers, hand) => {
+  const grid = [
+    [0, -2],
+    [-1, 1],
+    [0, 1],
+    [1, 1],
+    [-1, 0],
+    [0, 0],
+    [1, 0],
+    [-1, -1],
+    [0, -1],
+    [1, -1],
+    [-1, -2],
+    [1, -2],
+  ];
+  let answer = "";
+  let L = 10;
+  let R = 11;
+  let L_steps, R_steps;
+
+  hand = hand[0].toUpperCase();
+  numbers.forEach((el) => {
+    switch (grid[el][0]) {
+      case -1:
+        answer += "L";
+        L = el;
+        break;
+      case 1:
+        answer += "R";
+        R = el;
+        break;
+      case 0:
+        L_steps =
+          Math.abs(grid[L][0] - grid[el][0]) +
+          Math.abs(grid[L][1] - grid[el][1]);
+        R_steps =
+          Math.abs(grid[R][0] - grid[el][0]) +
+          Math.abs(grid[R][1] - grid[el][1]);
+
+        if (L_steps > R_steps) {
+          answer += "R";
+          R = el;
+        } else if (L_steps < R_steps) {
+          answer += "L";
+          L = el;
+        } else {
+          answer += hand;
+          eval(`${hand} = el`);
+        }
+    }
+  });
+
+  return answer;
+};
