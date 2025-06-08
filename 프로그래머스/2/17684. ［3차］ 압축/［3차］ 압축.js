f2844792d7bbd50b833d80c12b12930f0e6fea17
Hyeyeong Lee <128236313+kkotburi@users.noreply.github.com>
2025-06-08 23:59:13 +0900

    [level 2] Title: [3차] 압축, Time: 0.76 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.js
@@ -0,0 +1,29 @@
+const solution = (msg) => {
+  let answer = [];
+  let map = new Map();
+  let index = 27;
+  let w = "";
+  let c = "";
+  let temp = 0;
+
+  for (let i = 0; i < msg.length; i++) {
+    w = msg[i];
+    c = msg[i + 1];
+
+    if (!map.get(w + c)) {
+      map.set(w + c, index++);
+      answer.push(w.charCodeAt() - 64);
+    } else {
+      while (map.get(w + c)) {
+        temp = w + c;
+        w = w + c;
+        c = msg[++i + 1];
+      }
+
+      map.set(w + c, index++);
+      answer.push(map.get(temp));
+    }
+  }
+
+  return answer;
+};
