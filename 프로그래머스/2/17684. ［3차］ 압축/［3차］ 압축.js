0e0d67c1754d525cbc4e89defd22aec11566d46b
Hyeyeong Lee <128236313+kkotburi@users.noreply.github.com>
2025-06-10 23:59:56 +0900

    [level 2] Title: [3차] 압축, Time: 0.81 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.js
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.js
@@ -1,19 +1,48 @@
 const solution = (msg) => {
-  const answer = [];
-  const dict = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("").reduce((dict, c, i) => {
-    dict[c] = i + 1;
-    return dict;
-  }, {});
-  dict.nextId = 27;
+  let answer = [];
+  let list = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+  ];
+  let dic = list.reduce((d, a, i) => ((d[a] = i + 1), d), {});
 
-  for (let i = 0, j = 0; i < msg.length; i = j) {
-    j = msg.length;
-    let longest = "";
+  for (var i = 0; i < msg.length; i++) {
+    let w = msg[i];
+    let c = msg[i + 1];
 
-    while (dict[(longest = msg.substring(i, j))] === undefined) --j;
+    while (dic[w + c] && i < msg.length - 1) {
+      i++;
+      w += c;
+      c = msg[i + 1];
+    }
 
-    answer.push(dict[longest]);
-    dict[longest + msg[j]] = dict.nextId++;
+    answer.push(dic[w]);
+    list.push(dic[w + c]);
+    dic[w + c] = list.length;
   }
 
   return answer;
