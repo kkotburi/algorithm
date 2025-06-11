f253dcd955340ff23cbf18ef457d436976b604bd
Hyeyeong Lee <128236313+kkotburi@users.noreply.github.com>
2025-06-11 23:54:33 +0900

    [level 2] Title: [3차] 압축, Time: 0.77 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.js
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.js
@@ -1,49 +1,48 @@
 const solution = (msg) => {
-  let answer = [];
-  let list = [
-    "A",
-    "B",
-    "C",
-    "D",
-    "E",
-    "F",
-    "G",
-    "H",
-    "I",
-    "J",
-    "K",
-    "L",
-    "M",
-    "N",
-    "O",
-    "P",
-    "Q",
-    "R",
-    "S",
-    "T",
-    "U",
-    "V",
-    "W",
-    "X",
-    "Y",
-    "Z",
-  ];
-  let dic = list.reduce((d, a, i) => ((d[a] = i + 1), d), {});
+  const answer = [];
+  const dic = {
+    A: 1,
+    B: 2,
+    C: 3,
+    D: 4,
+    E: 5,
+    F: 6,
+    G: 7,
+    H: 8,
+    I: 9,
+    J: 10,
+    K: 11,
+    L: 12,
+    M: 13,
+    N: 14,
+    O: 15,
+    P: 16,
+    Q: 17,
+    R: 18,
+    S: 19,
+    T: 20,
+    U: 21,
+    V: 22,
+    W: 23,
+    X: 24,
+    Y: 25,
+    Z: 26,
+  };
+  let next = "";
+  let index = 27;
 
-  for (var i = 0; i < msg.length; i++) {
-    let w = msg[i];
-    let c = msg[i + 1];
+  const s = msg.split("").reduce((acc, cur) => {
+    next = acc + cur;
 
-    while (dic[w + c] && i < msg.length - 1) {
-      i++;
-      w += c;
-      c = msg[i + 1];
-    }
+    if (dic[next] === undefined) {
+      dic[next] = index++;
+    } else return acc + cur;
 
-    answer.push(dic[w]);
-    list.push(dic[w + c]);
-    dic[w + c] = list.length;
-  }
+    if (dic[acc] !== undefined) answer.push(dic[acc]);
+    return cur;
+  });
+
+  answer.push(dic[s]);
 
   return answer;
 };
