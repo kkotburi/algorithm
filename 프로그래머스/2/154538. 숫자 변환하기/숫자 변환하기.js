9ca3ccf0c1280085efb27983296e8686746e81ee
Hyeyeong Lee <128236313+kkotburi@users.noreply.github.com>
2025-06-16 23:47:51 +0900

    [level 2] Title: 숫자 변환하기, Time: 2604.14 ms, Memory: 68.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.js
@@ -0,0 +1,14 @@
+const solution = (x, y, n) => {
+  let que = [[y, 0]];
+
+  while (que.length) {
+    const [num, i] = que.shift();
+
+    if (num === x) return i;
+    if (num % 2 === 0 && num / 2 >= x) que.push([num / 2, i + 1]);
+    if (num % 3 === 0 && num / 3 >= x) que.push([num / 3, i + 1]);
+    if (num - n >= x) que.push([num - n, i + 1]);
+  }
+
+  return -1;
+};
