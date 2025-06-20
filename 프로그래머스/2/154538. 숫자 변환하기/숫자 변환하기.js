c34ae5162669e9b347f1a08c8b457a8618b109db
Hyeyeong Lee <128236313+kkotburi@users.noreply.github.com>
2025-06-20 23:58:31 +0900

    [level 2] Title: 숫자 변환하기, Time: 229.97 ms, Memory: 88 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.js
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.js
@@ -1,25 +1,22 @@
 const solution = (x, y, n) => {
-  const queue = [[x, 0]];
-  const done = {};
+  let answer = 0;
+  let test = [x];
 
-  let [l, r] = [0, 1];
-  let answer = Infinity;
+  if (x === y) return 0;
 
-  while (l < r) {
-    const [num, count] = queue[l++];
+  while (true) {
+    const set = new Set();
+    answer++;
 
-    if (done[num] || num > y) continue;
-    if (num === y) {
-      answer = Math.min(answer, count);
-      continue;
-    }
+    test.forEach((item) => {
+      if (item + n <= y) set.add(item + n);
+      if (item * 2 <= y) set.add(item * 2);
+      if (item * 3 <= y) set.add(item * 3);
+    });
 
-    done[num] = true;
+    if (set.size === 0) return -1;
+    if (set.has(y)) return answer;
 
-    queue[r++] = [num * 3, count + 1];
-    queue[r++] = [num * 2, count + 1];
-    queue[r++] = [num + n, count + 1];
+    test = set;
   }
-
-  return answer === Infinity ? -1 : answer;
 };
