860eba4e1e78cb5c3dd7dd49010f7cb81e32bbf6
Hyeyeong Lee <128236313+kkotburi@users.noreply.github.com>
2025-06-23 23:08:20 +0900

    [level 2] Title: 가장 큰 수, Time: 170.16 ms, Memory: 44.7 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
@@ -1,8 +1,4 @@
 const solution = (numbers) => {
-  let answer = numbers
-    .map((v) => String(v))
-    .sort((a, b) => b + a - (a + b))
-    .join("");
-
+  let answer = numbers.sort((a, b) => `${b}${a}` - `${a}${b}`).join("");
   return answer[0] === "0" ? "0" : answer;
 };
