3b94534e6dd0325fcad5fd74e1396fd56eee2aa5
Hyeyeong Lee <128236313+kkotburi@users.noreply.github.com>
2025-06-25 23:39:36 +0900

    [level 2] Title: 가장 큰 수, Time: 174.91 ms, Memory: 44.8 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/README.md
+++ 프로그래머스/2/42746. 가장 큰 수/README.md
@@ -1,10 +1,10 @@
 # [level 2] 가장 큰 수 - 42746 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42746?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42746) 
 
 ### 성능 요약
 
-메모리: 46.1 MB, 시간: 29.00 ms
+메모리: 44.8 MB, 시간: 174.91 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 06월 24일 23:55:03
+2025년 06월 25일 23:39:33
 
 ### 문제 설명
 
--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
@@ -1,20 +1,8 @@
 const solution = (numbers) => {
-  const buckets = [];
-
-  for (const n of numbers) {
-    const s = String(n);
-    const l = s.length;
-    let j = "";
-
-    for (let i = 0; i < 4; i++) {
-      j += s[l > i ? i : i % l];
-    }
-
-    j = 9999 - j;
-    buckets[j] = buckets[j] ? buckets[j] + s : s;
-  }
-
-  const answer = buckets.join``;
+  let answer = numbers
+    .map((v) => String(v))
+    .sort((a, b) => b + a - (a + b))
+    .join("");
 
   return answer[0] === "0" ? "0" : answer;
 };
