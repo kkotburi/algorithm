847b22e1be111d7d36b7084cccebd41bdc250748
Hyeyeong Lee <128236313+kkotburi@users.noreply.github.com>
2025-07-03 22:47:23 +0900

    [level 2] Title: 프로세스, Time: 0.82 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/README.md
+++ 프로그래머스/2/42587. 프로세스/README.md
@@ -1,10 +1,10 @@
 # [level 2] 프로세스 - 42587 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42587) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42587?language=javascript) 
 
 ### 성능 요약
 
-메모리: 33.6 MB, 시간: 0.75 ms
+메모리: 33.6 MB, 시간: 0.82 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 03일 22:46:21
+2025년 07월 03일 22:47:22
 
 ### 문제 설명
 
--- 프로그래머스/2/42587. 프로세스/프로세스.js
+++ 프로그래머스/2/42587. 프로세스/프로세스.js
@@ -1,16 +1,17 @@
 const solution = (priorities, location) => {
+  let answer = [];
   let array = priorities.map((priority, index) => ({ index, priority }));
-  let queue = [];
 
   while (array.length > 0) {
     let first = array.shift();
     let priority = array.some((v) => v.priority > first.priority);
+
     if (priority) {
       array.push(first);
     } else {
-      queue.push(first);
+      answer.push(first);
     }
   }
 
-  return queue.findIndex((v) => v.index === location) + 1;
+  return answer.findIndex((v) => v.index === location) + 1;
 };
