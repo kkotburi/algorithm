6ce5929b23d075d489abc41dc5a3f61a9e068e52
Hyeyeong Lee <128236313+kkotburi@users.noreply.github.com>
2025-07-08 23:45:58 +0900

    [level 0] Title: 옹알이 (1), Time: 0.22 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.js
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.js
@@ -1,11 +1,10 @@
 const solution = (babbling) => {
-  for (let i = 0; i < babbling.length; i++) {
-    babbling[i] = babbling[i]
-      .replace("aya", " ")
-      .replace("ye", " ")
-      .replace("woo", " ")
-      .replace("ma", " ");
-  }
-  return babbling.map((v) => v.replace(/ /g, "")).filter((v) => v === "")
-    .length;
+  let answer = 0;
+  const regex = /^(aya|ye|woo|ma)+$/;
+
+  babbling.forEach((word) => {
+    if (regex.test(word)) answer++;
+  });
+
+  return answer;
 };
