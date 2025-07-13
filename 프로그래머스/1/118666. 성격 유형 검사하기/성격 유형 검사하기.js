6c2ca2d488ad8da75ce7cc919a0c69fedbf77f20
Hyeyeong Lee <128236313+kkotburi@users.noreply.github.com>
2025-07-13 23:57:34 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.53 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
@@ -1,14 +1,13 @@
 const solution = (survey, choices) => {
-  let answer = "";
-  let indicators = { R: 0, T: 0, C: 0, F: 0, J: 0, M: 0, A: 0, N: 0 };
-  for (let i = 0; i < survey.length; i++) {
-    let type = survey[i].split("")[choices[i] < 4 ? 0 : 1];
-    indicators[type] += Math.abs(choices[i] - 4);
-  }
-  for (let j = 0; j < Object.keys(indicators).length; j += 2) {
-    Object.values(indicators)[j] >= Object.values(indicators)[j + 1]
-      ? (answer += Object.keys(indicators)[j])
-      : (answer += Object.keys(indicators)[j + 1]);
-  }
-  return answer;
+  let MBTI = {};
+  let types = ["RT", "CF", "JM", "AN"];
+
+  types.forEach((type) => type.split("").forEach((char) => (MBTI[char] = 0)));
+
+  choices.forEach((choice, index) => {
+    let [disagree, agree] = survey[index];
+    MBTI[choice > 4 ? agree : disagree] += Math.abs(choice - 4);
+  });
+
+  return types.map(([a, b]) => (MBTI[b] > MBTI[a] ? b : a)).join("");
 };
