1a77a57ffc88103e898e514ab1fb71b8413d9e08
Hyeyeong Lee <128236313+kkotburi@users.noreply.github.com>
2025-07-15 02:18:05 +0900

    [level 1] Title: 체육복, Time: 0.17 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/42862. 체육복/체육복.js
@@ -0,0 +1,20 @@
+const solution = (n, lost, reserve) => {
+  const filterdLost = lost.filter((item) => !reserve.includes(item)).sort();
+  const filterdReserve = reserve.filter((item) => !lost.includes(item)).sort();
+
+  for (let i = 0; i < filterdLost.length; i++) {
+    for (let j = 0; j < filterdReserve.length; j++) {
+      if (
+        filterdLost[i] === filterdReserve[j] + 1 ||
+        filterdLost[i] === filterdReserve[j] - 1
+      ) {
+        filterdReserve.splice(j, 1);
+        filterdLost.splice(i, 1);
+        j--;
+        i--;
+      }
+    }
+  }
+
+  return n - filterdLost.length;
+};
