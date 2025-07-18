511b4df54bf71590d73f44f046555b35643efc48
Hyeyeong Lee <128236313+kkotburi@users.noreply.github.com>
2025-07-18 23:45:03 +0900

    [level 1] Title: 둘만의 암호, Time: 0.22 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.js
@@ -0,0 +1,35 @@
+const solution = (s, skip, index) => {
+  const alphabet = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ].filter((v) => !skip.includes(v));
+
+  return s
+    .split("")
+    .map((v) => alphabet[(alphabet.indexOf(v) + index) % alphabet.length])
+    .join("");
+};
