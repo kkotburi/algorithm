39907e40459a6c91e03ba4f86dd642ce33d7fd40
Hyeyeong Lee <128236313+kkotburi@users.noreply.github.com>
2025-07-26 23:54:10 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.js
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.js
@@ -1,10 +1,10 @@
+const GreatestCommonDivisor = (a, b) =>
+  a % b ? GreatestCommonDivisor(b, a % b) : b;
+
 const solution = (numer1, denom1, numer2, denom2) => {
-  let answer = [numer1 * denom2 + numer2 * denom1, denom1 * denom2];
-  for (let i = 2; i <= answer[1]; i++) {
-    if (!(answer[0] % i) && !(answer[1] % i)) {
-      answer = [answer[0] / i, answer[1] / i];
-      i--;
-    }
-  }
-  return answer;
+  let denum = numer1 * denom2 + numer2 * denom1;
+  let num = denom1 * denom2;
+  let gcd = GreatestCommonDivisor(denum, num);
+
+  return [denum / gcd, num / gcd];
 };
