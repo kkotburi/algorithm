43eebf6ebb7cc18be65e8ee98b5372b6e9c713a7
Hyeyeong Lee <128236313+kkotburi@users.noreply.github.com>
2025-07-28 23:28:26 +0900

    [level 0] Title: 주사위 게임 3, Time: 0.18 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.js
+++ 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.js
@@ -1,22 +1,23 @@
 const solution = (a, b, c, d) => {
   let map = new Map();
+
   for (const num of [a, b, c, d]) {
     map.set(num, (map.get(num) || 0) + 1);
   }
+
   let num = [...map.keys()].sort((a, b) => map.get(b) - map.get(a));
   let [p, q, r, s] = num;
 
-  if (num.length === 1) {
-    return p * 1111;
+  switch (num.length) {
+    case 1:
+      return p * 1111;
+    case 2:
+      return Math.max(...map.values()) === 3
+        ? (10 * p + q) ** 2
+        : (p + q) * Math.abs(p - q);
+    case 3:
+      return q * r;
+    default:
+      return Math.min(p, q, r, s);
   }
-  if (num.length === 2) {
-    return Math.max(...map.values()) === 3
-      ? (10 * p + q) ** 2
-      : (p + q) * Math.abs(p - q);
-  }
-  if (num.length === 3) {
-    return q * r;
-  }
-
-  return Math.min(p, q, r, s);
 };
