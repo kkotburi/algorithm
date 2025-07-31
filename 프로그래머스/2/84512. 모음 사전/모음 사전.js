a269b1dfb6f9a5d9592a16c92ba7f89ee0fd4ad9
Hyeyeong Lee <128236313+kkotburi@users.noreply.github.com>
2025-07-31 23:25:42 +0900

    [level 2] Title: 모음 사전, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.js
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.js
@@ -1,11 +1,8 @@
-const solution = (word) => {
-  let answer = 0;
-  const vowels = ["A", "E", "I", "O", "U"];
-  const counts = [781, 156, 31, 6, 1];
-
-  for (let i = 0; i < word.length; i++) {
-    answer += vowels.indexOf(word[i]) * counts[i] + 1;
-  }
-
-  return answer;
-};
+const solution = (word) =>
+  word
+    .split("")
+    .reduce(
+      (a, c, i) =>
+        a + [781, 156, 31, 6, 1][i] * ["A", "E", "I", "O", "U"].indexOf(c) + 1,
+      0
+    );
