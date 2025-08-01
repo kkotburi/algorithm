f4dff8066f4d904cdd49db27499e440be20af64c
Hyeyeong Lee <128236313+kkotburi@users.noreply.github.com>
2025-08-01 23:34:48 +0900

    [level 0] Title: 안전지대, Time: 0.35 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120866. 안전지대/안전지대.js
+++ 프로그래머스/0/120866. 안전지대/안전지대.js
@@ -1,21 +1,25 @@
 const solution = (board) => {
   let answer = 0;
-  for (let i = 0; i < board.length; i++) {
-    for (let j = 0; j < board[i].length; j++) {
+  let danger = [
+    [-1, 0],
+    [-1, -1],
+    [-1, 1],
+    [0, -1],
+    [0, 1],
+    [1, 0],
+    [1, -1],
+    [1, 1],
+  ];
+
+  board.forEach((row, column, array) =>
+    row.forEach((element, index) => {
       if (
-        !board[i - 1]?.[j - 1] &&
-        !board[i - 1]?.[j] &&
-        !board[i - 1]?.[j + 1] &&
-        !board[i][j - 1] &&
-        !board[i][j] &&
-        !board[i][j + 1] &&
-        !board[i + 1]?.[j - 1] &&
-        !board[i + 1]?.[j] &&
-        !board[i + 1]?.[j + 1]
-      ) {
+        element !== 1 &&
+        !danger.some(([y, x]) => !!array[column + y]?.[index + x])
+      )
         answer++;
-      }
-    }
-  }
+    })
+  );
+
   return answer;
 };
