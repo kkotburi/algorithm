0f15ab0ed7c1f331c4487b482b9fc6744a61b586
Hyeyeong Lee <128236313+kkotburi@users.noreply.github.com>
2025-08-03 23:52:05 +0900

    [level 1] Title: 과일 장수, Time: 222.15 ms, Memory: 89.3 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.js
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.js
@@ -1,14 +1,13 @@
 const solution = (k, m, score) => {
-  score.sort((a, b) => b - a);
-  let boxes = [];
-  for (let i = 0; i < score.length; i += m) {
-    boxes.push(score.slice(i, i + m));
-  }
   let answer = 0;
-  for (let j = 0; j < boxes.length; j++) {
-    if (boxes[j][m - 1]) {
-      answer += boxes[j][m - 1] * m;
-    }
+  let sortedScore = score
+    .slice()
+    .sort((a, b) => a - b)
+    .slice(score.length % m);
+
+  for (let i = 0; i < sortedScore.length; i += m) {
+    answer += sortedScore[i] * m;
   }
+
   return answer;
 };
