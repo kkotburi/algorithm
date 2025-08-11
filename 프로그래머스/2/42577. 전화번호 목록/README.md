0f74b826fb91ffa423165b24dc7c8c4084e2ee70
Hyeyeong Lee <128236313+kkotburi@users.noreply.github.com>
2025-08-11 23:53:22 +0900

    [level 2] Title: 전화번호 목록, Time: 155.31 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/README.md
+++ 프로그래머스/2/42577. 전화번호 목록/README.md
@@ -1,10 +1,10 @@
 # [level 2] 전화번호 목록 - 42577 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577?language=javascript) 
 
 ### 성능 요약
 
-메모리: 78.3 MB, 시간: 153.87 ms
+메모리: 78.2 MB, 시간: 155.31 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 08월 10일 16:26:45
+2025년 08월 11일 23:53:19
 
 ### 문제 설명
 
--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -1,5 +1,9 @@
-const solution = (phone_book) =>
-  !phone_book.sort().some((_, i) => {
-    if (i === phone_book.length - 1) return false;
-    return phone_book[i + 1].startsWith(phone_book[i]);
-  });
+const solution = (phone_book) => {
+  phone_book.sort();
+
+  for (let i = 0; i < phone_book.length - 1; i++) {
+    if (phone_book[i + 1].indexOf(phone_book[i]) === 0) return false;
+  }
+
+  return true;
+};
