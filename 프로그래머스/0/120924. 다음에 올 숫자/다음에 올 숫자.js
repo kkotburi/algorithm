ba26b033ac6b7655f7341209a48bd83c95426756
Hyeyeong Lee <128236313+kkotburi@users.noreply.github.com>
2025-08-13 23:56:30 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.js
+++ 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.js
@@ -1,8 +1,4 @@
-const solution = (common) => {
-  if (common[1] - common[0] === common[2] - common[1]) {
-    return common[common.length - 1] + common[1] - common[0];
-  }
-  if (common[1] / common[0] === common[2] / common[1]) {
-    return common[common.length - 1] * (common[1] / common[0]);
-  }
-};
+const solution = (common) =>
+  common[1] - common[0] === common[2] - common[1]
+    ? common.pop() + common[1] - common[0]
+    : common.pop() * (common[1] / common[0]);
