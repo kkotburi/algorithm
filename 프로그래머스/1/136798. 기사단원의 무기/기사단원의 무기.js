da9165e8d60f49e34286f5712a536cd28575dbdd
Hyeyeong Lee <128236313+kkotburi@users.noreply.github.com>
2025-08-15 21:36:48 +0900

    [level 1] Title: 기사단원의 무기, Time: 146.00 ms, Memory: 37.4 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.js
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.js
@@ -1,7 +1,9 @@
 const solution = (number, limit, power) => {
   let answer = 0;
+
   for (let i = 1; i <= number; i++) {
     let divisor = 0;
+
     for (let j = 1; j <= Math.sqrt(i); j++) {
       if (j ** 2 === i) {
         divisor++;
@@ -9,7 +11,9 @@ const solution = (number, limit, power) => {
         divisor += 2;
       }
     }
+
     divisor > limit ? (answer += power) : (answer += divisor);
   }
+
   return answer;
 };
