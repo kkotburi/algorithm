f2c5d122faf3097985fb1a3df6c928482294b47f
Hyeyeong Lee <128236313+kkotburi@users.noreply.github.com>
2025-08-17 03:31:55 +0900

    [level 1] Title: 폰켓몬, Time: 0.59 ms, Memory: 34.1 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -1,5 +1,5 @@
 const solution = (nums) => {
   let numsSet = [...new Set(nums)];
-  let numsHalf = Math.floor(nums.length / 2);
+  let numsHalf = nums.length / 2;
   return numsSet.length > numsHalf ? numsHalf : numsSet.length;
 };
