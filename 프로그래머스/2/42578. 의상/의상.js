506fde509489b5001a31a007f51b4cf4edc1c5c0
Hyeyeong Lee <128236313+kkotburi@users.noreply.github.com>
2025-08-17 22:12:34 +0900

    [level 2] Title: 의상, Time: 0.19 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.js
+++ 프로그래머스/2/42578. 의상/의상.js
@@ -1,7 +1,7 @@
 const solution = (clothes) =>
   Object.values(
-    clothes.reduce((acc, obj) => {
-      acc[obj[1]] = acc[obj[1]] ? acc[obj[1]] + 1 : 1;
+    clothes.reduce((acc, cur) => {
+      acc[cur[1]] = acc[cur[1]] ? acc[cur[1]] + 1 : 1;
       return acc;
     }, {})
   ).reduce((a, b) => a * (b + 1), 1) - 1;
