84e8036e8720e58bd8e306dbab5cb1e3fca6d976
Hyeyeong Lee <128236313+kkotburi@users.noreply.github.com>
2025-08-21 23:12:33 +0900

    [level 0] Title: OX퀴즈, Time: 0.34 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120907. OX퀴즈/OX퀴즈.js
+++ 프로그래머스/0/120907. OX퀴즈/OX퀴즈.js
@@ -1,19 +1,2 @@
-const solution = (quiz) => {
-  let answer = [];
-  for (let i = 0; i < quiz.length; i++) {
-    quiz[i] = quiz[i].split(" ");
-    switch (quiz[i][1]) {
-      case "+":
-        Number(quiz[i][0]) + Number(quiz[i][2]) === Number(quiz[i][4])
-          ? answer.push("O")
-          : answer.push("X");
-        break;
-      case "-":
-        Number(quiz[i][0]) - Number(quiz[i][2]) === Number(quiz[i][4])
-          ? answer.push("O")
-          : answer.push("X");
-        break;
-    }
-  }
-  return answer;
-};
+const solution = (quiz) =>
+  quiz.map((v) => v.split(" = ")).map((v) => (eval(v[0]) == v[1] ? "O" : "X"));
--- 프로그래머스/0/120907. OX퀴즈/README.md
+++ 프로그래머스/0/120907. OX퀴즈/README.md
@@ -1,10 +1,10 @@
 # [level 0] OX퀴즈 - 120907 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120907) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120907?language=javascript) 
 
 ### 성능 요약
 
-메모리: 33.6 MB, 시간: 0.07 ms
+메모리: 33.5 MB, 시간: 0.34 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 21일 23:48:26
+2025년 08월 21일 23:12:30
 
 ### 문제 설명
 
