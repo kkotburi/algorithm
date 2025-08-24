eae9b266f994d5fcd91c7ad0107e2d814773029a
Hyeyeong Lee <128236313+kkotburi@users.noreply.github.com>
2025-08-24 23:31:42 +0900

    [level 1] Title: 추억 점수, Time: 3.62 ms, Memory: 36 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/README.md
+++ 프로그래머스/1/176963. 추억 점수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 추억 점수 - 176963 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/176963) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/176963?language=javascript) 
 
 ### 성능 요약
 
-메모리: 33.4 MB, 시간: 0.05 ms
+메모리: 36 MB, 시간: 3.62 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 19일 22:12:55
+2025년 08월 24일 23:31:40
 
 ### 문제 설명
 
--- 프로그래머스/1/176963. 추억 점수/추억 점수.js
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.js
@@ -1,13 +1,4 @@
-const solution = (name, yearning, photo) => {
-  let answer = [];
-  for (let i = 0; i < photo.length; i++) {
-    let sum = 0;
-    for (let j = 0; j < photo[i].length; j++) {
-      if (name.indexOf(photo[i][j]) !== -1) {
-        sum += yearning[name.indexOf(photo[i][j])];
-      }
-    }
-    answer.push(sum);
-  }
-  return answer;
-};
+const solution = (name, yearning, photo) =>
+  photo.map((v) =>
+    v.reduce((a, c) => (a += yearning[name.indexOf(c)] ?? 0), 0)
+  );
