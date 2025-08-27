61c617b4cdf13e857dc8671fff86d74ffaa0a9e9
Hyeyeong Lee <128236313+kkotburi@users.noreply.github.com>
2025-08-27 22:18:16 +0900

    [level 1] Title: 명예의 전당 (1), Time: 6.40 ms, Memory: 36.6 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/README.md
+++ 프로그래머스/1/138477. 명예의 전당 （1）/README.md
@@ -1,10 +1,10 @@
 # [level 1] 명예의 전당 (1) - 138477 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/138477) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/138477?language=javascript) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.06 ms
+메모리: 36.6 MB, 시간: 6.40 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 08일 23:54:18
+2025년 08월 27일 22:18:11
 
 ### 문제 설명
 
--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.js
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.js
@@ -1,14 +1,8 @@
 const solution = (k, score) => {
-  let hall = [];
   let answer = [];
-  for (let i = 0; i < score.length; i++) {
-    if (hall.length < k) {
-      hall.push(score[i]);
-    } else if (score[i] > Math.min(...hall)) {
-      hall.push(score[i]);
-      hall.sort((a, b) => b - a).pop();
-    }
-    answer.push(Math.min(...hall));
-  }
-  return answer;
+  return score.reduce((acc, cur) => {
+    answer.push(cur);
+    answer = answer.sort((a, b) => b - a).slice(0, k);
+    return [...acc, Math.min(...answer)];
+  }, []);
 };
