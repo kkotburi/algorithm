f94f74d4c941d1164a2294b22191df2842fe6fe3
Hyeyeong Lee <128236313+kkotburi@users.noreply.github.com>
2025-08-30 23:58:09 +0900

    [level 0] Title: 코드 처리하기, Time: 6.83 ms, Memory: 39 MB -BaekjoonHub

--- 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.js
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.js
@@ -1,12 +1,8 @@
 const solution = (code) => {
-  let mode = 0;
-  let answer = "";
-  for (let i = 0; i < code.length; i++) {
-    if (code[i] === "1") {
-      mode ? mode-- : mode++;
-    } else if (i % 2 == mode) {
-      answer += code[i];
-    }
-  }
-  return answer === "" ? "EMPTY" : answer;
+  let answer = code
+    .replaceAll("1", "")
+    .split("")
+    .filter((_, i) => i % 2 === 0)
+    .join("");
+  return answer.length > 0 ? answer : "EMPTY";
 };
