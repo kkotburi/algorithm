13eee3e8203dcd6e1562049eb4c03934d08c3356
Hyeyeong Lee <128236313+kkotburi@users.noreply.github.com>
2025-09-03 23:58:11 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 1.42 ms, Memory: 36.6 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/README.md
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/README.md
@@ -1,10 +1,10 @@
 # [level 1] 가장 가까운 같은 글자 - 142086 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/142086) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/142086?language=javascript) 
 
 ### 성능 요약
 
-메모리: 37.6 MB, 시간: 9.25 ms
+메모리: 36.6 MB, 시간: 1.42 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 01일 15:37:27
+2025년 09월 03일 23:58:08
 
 ### 문제 설명
 
--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
@@ -1,14 +1,8 @@
 const solution = (s) => {
-  let answer = [];
-  for (let i = 0; i < s.length; i++) {
-    for (let j = i - 1; j >= -1; j--) {
-      if (s[i] === s[j]) {
-        answer.push(i - j);
-        break;
-      } else if (j === -1) {
-        answer.push(-1);
-      }
-    }
-  }
-  return answer;
+  let hash = {};
+  return [...s].map((v, i) => {
+    let answer = hash[v] !== undefined ? i - hash[v] : -1;
+    hash[v] = i;
+    return answer;
+  });
 };
