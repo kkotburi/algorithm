5720f7fd17e8ccc5c076160d5286b8206699d4fd
Hyeyeong Lee <128236313+kkotburi@users.noreply.github.com>
2025-09-09 23:56:53 +0900

    [level 0] Title: 문자열 밀기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.js
+++ 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.js
@@ -1,10 +1,1 @@
-const solution = (A, B) => {
-  let answer = -1;
-  for (let i = 1; i < A.length; i++) {
-    if (B === A.substr(-i) + A.substr(0, A.length - i)) {
-      answer = i;
-      break;
-    }
-  }
-  return A === B ? 0 : answer;
-};
+const solution = (a, b) => (b + b).indexOf(a);
