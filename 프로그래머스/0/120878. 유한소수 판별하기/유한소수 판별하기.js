8a90cdb93a21d29106f3a78167cd3d00c2f0d640
Hyeyeong Lee <128236313+kkotburi@users.noreply.github.com>
2025-09-10 23:55:37 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.js
+++ 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.js
@@ -1,14 +1,1 @@
-const solution = (a, b) => {
-  for (let i = Math.min(a, b); i > 1; i--) {
-    if (!(a % i) && !(b % i)) {
-      b /= i;
-      break;
-    }
-  }
-  for (let j = 2; j <= b; j++) {
-    if (j % 2 && j % 5 && !(b % j)) {
-      return 2;
-    }
-  }
-  return 1;
-};
+const solution = (a, b) => (Number((a / b).toFixed(10)) == a / b ? 1 : 2);
