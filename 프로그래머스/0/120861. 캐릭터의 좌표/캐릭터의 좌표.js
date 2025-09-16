15d081338a0af841a00180539a995b22c395ea0d
Hyeyeong Lee <128236313+kkotburi@users.noreply.github.com>
2025-09-16 23:57:19 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.32 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.js
+++ 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.js
@@ -1,22 +1,20 @@
 const solution = (keyinput, board) => {
-  let answer = [0, 0];
+  let key = { right: [1, 0], up: [0, 1], down: [0, -1], left: [-1, 0] };
 
-  for (let p of keyinput) {
-    switch (p) {
-      case "left":
-        if (-answer[0] < board[0] / 2 - 1) answer[0]--;
-        break;
-      case "right":
-        if (answer[0] < board[0] / 2 - 1) answer[0]++;
-        break;
-      case "up":
-        if (answer[1] < board[1] / 2 - 1) answer[1]++;
-        break;
-      case "down":
-        if (-answer[1] < board[1] / 2 - 1) answer[1]--;
-        break;
-    }
-  }
+  let answer = keyinput
+    .map((v) => key[v])
+    .reduce(
+      (a, c) => {
+        if (
+          Math.abs(a[0] + c[0]) > board[0] / 2 ||
+          Math.abs(a[1] + c[1]) > board[1] / 2
+        ) {
+          return [a[0], a[1]];
+        }
+        return [a[0] + c[0], a[1] + c[1]];
+      },
+      [0, 0]
+    );
 
   return answer;
 };
