8f7d9ec1eea47181b681497367f09e6a3446a486
Hyeyeong Lee <128236313+kkotburi@users.noreply.github.com>
2025-09-19 23:52:10 +0900

    [level 0] Title: 로그인 성공?, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.js
+++ 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.js
@@ -1,12 +1,5 @@
 const solution = (id_pw, db) => {
-  let answer = "fail";
-  for (let i = 0; i < db.length; i++) {
-    if (db[i][0] === id_pw[0] && db[i][1] === id_pw[1]) {
-      answer = "login";
-      break;
-    } else if (db[i][0] === id_pw[0]) {
-      answer = "wrong pw";
-    }
-  }
-  return answer;
+  let [id, pw] = id_pw;
+  let map = new Map(db);
+  return map.has(id) ? (map.get(id) === pw ? "login" : "wrong pw") : "fail";
 };
