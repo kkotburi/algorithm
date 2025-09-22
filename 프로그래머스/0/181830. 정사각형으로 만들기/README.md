c1ab4bbd3a93ef566e51a10b1fefe441c670bb99
Hyeyeong Lee <128236313+kkotburi@users.noreply.github.com>
2025-09-22 23:00:13 +0900

    [level 0] Title: 정사각형으로 만들기, Time: 0.54 ms, Memory: 38.3 MB -BaekjoonHub

--- 프로그래머스/0/181830. 정사각형으로 만들기/README.md
+++ 프로그래머스/0/181830. 정사각형으로 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 정사각형으로 만들기 - 181830 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181830) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181830?language=javascript) 
 
 ### 성능 요약
 
-메모리: 38.4 MB, 시간: 0.18 ms
+메모리: 38.3 MB, 시간: 0.54 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 03일 23:33:38
+2025년 09월 22일 23:00:09
 
 ### 문제 설명
 
--- 프로그래머스/0/181830. 정사각형으로 만들기/정사각형으로 만들기.js
+++ 프로그래머스/0/181830. 정사각형으로 만들기/정사각형으로 만들기.js
@@ -1,12 +1,6 @@
 const solution = (arr) => {
-  let length = Math.max(arr.length, arr[0].length);
-  let answer = [];
-  for (let i = 0; i < length; i++) {
-    if (arr[i]) {
-      answer.push([...arr[i], ...new Array(length - arr[i].length).fill(0)]);
-    } else {
-      answer.push(new Array(length).fill(0));
-    }
-  }
-  return answer;
+  let num = Math.max(arr.length, Math.max(...arr.map((v) => v.length)));
+  for (let a of arr) while (a.length < num) a.push(0);
+  while (arr.length < num) arr.push(Array(num).fill(0));
+  return arr;
 };
