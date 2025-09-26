3ea1b8f2e9319e64b901363a90f9c4c717878535
Hyeyeong Lee <128236313+kkotburi@users.noreply.github.com>
2025-09-26 23:55:26 +0900

    [level 0] Title: 그림 확대, Time: 1.26 ms, Memory: 34.1 MB -BaekjoonHub

--- 프로그래머스/0/181836. 그림 확대/README.md
+++ 프로그래머스/0/181836. 그림 확대/README.md
@@ -1,10 +1,10 @@
 # [level 0] 그림 확대 - 181836 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181836) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181836?language=javascript) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.07 ms
+메모리: 34.1 MB, 시간: 1.26 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 05일 23:58:38
+2025년 09월 26일 23:55:24
 
 ### 문제 설명
 
--- 프로그래머스/0/181836. 그림 확대/그림 확대.js
+++ 프로그래머스/0/181836. 그림 확대/그림 확대.js
@@ -1,8 +1,4 @@
-const solution = (picture, k) => {
-  let answer = [];
-  picture.forEach((v) => {
-    const expanded = [...v].reduce((acc, curr) => acc + curr.repeat(k), "");
-    for (let i = 0; i < k; i++) answer.push(expanded);
-  });
-  return answer;
-};
+const solution = (picture, k) =>
+  Array(picture.length * k)
+    .fill(0)
+    .map((_, i) => picture[~~(i / k)].replace(/./g, (v) => v.repeat(k)));
