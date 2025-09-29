464148ab868490fcede20a07b88b4540d0567d41
Hyeyeong Lee <128236313+kkotburi@users.noreply.github.com>
2025-09-29 23:58:17 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.09 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.js
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.js
@@ -1,12 +1,20 @@
-const solution = (numbers) =>
-  +numbers
-    .replace(/zero/g, "0")
-    .replace(/one/g, "1")
-    .replace(/two/g, "2")
-    .replace(/three/g, "3")
-    .replace(/four/g, "4")
-    .replace(/five/g, "5")
-    .replace(/six/g, "6")
-    .replace(/seven/g, "7")
-    .replace(/eight/g, "8")
-    .replace(/nine/g, "9");
+const solution = (numbers) => {
+  const number = [
+    "zero",
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+  ];
+
+  for (let i = 0; i < number.length; i++) {
+    numbers = numbers.split(number[i]).join(i);
+  }
+
+  return +numbers;
+};
