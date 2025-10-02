153cd326a17babceeb065034b58e1fc51db82945
Hyeyeong Lee <128236313+kkotburi@users.noreply.github.com>
2025-10-02 22:18:14 +0900

    [level 0] Title: 조건 문자열, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181934. 조건 문자열/조건 문자열.js
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.js
@@ -1,12 +1,6 @@
 const solution = (ineq, eq, n, m) => {
-  switch (ineq + eq) {
-    case "<=":
-      return n <= m ? 1 : 0;
-    case ">=":
-      return n >= m ? 1 : 0;
-    case "<!":
-      return n < m ? 1 : 0;
-    case ">!":
-      return n > m ? 1 : 0;
-  }
+  if (eq === "=" && n === m) return 1;
+  if (ineq === "<" && n < m) return 1;
+  if (ineq === ">" && n > m) return 1;
+  return 0;
 };
