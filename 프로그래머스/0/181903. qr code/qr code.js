b5f83e1e685e5ea9a7e61f50f8043be34b9f2ada
Hyeyeong Lee <128236313+kkotburi@users.noreply.github.com>
2025-10-13 18:11:28 +0900

    [level 0] Title: qr code, Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181903. qr code/qr code.js
+++ 프로그래머스/0/181903. qr code/qr code.js
@@ -1,7 +1,2 @@
-const solution = (q, r, code) => {
-  let answer = "";
-  for (let i = r; i < code.length; i += q) {
-    answer += code[i];
-  }
-  return answer;
-};
+const solution = (q, r, code) =>
+  [...code].filter((_, i) => i % q === r).join("");
