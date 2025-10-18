af476d8d20f7cef25d35b705e7f3d0bc2ea06996
Hyeyeong Lee <128236313+kkotburi@users.noreply.github.com>
2025-10-18 16:36:12 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.js
+++ 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.js
@@ -1,10 +1,5 @@
 const solution = (s) => {
-  s = [...s];
   let answer = [];
-  for (let i = 0; i < s.length; i++) {
-    if (s.filter((v) => v === s[i]).length === 1) {
-      answer.push(s[i]);
-    }
-  }
+  for (let c of s) if (s.indexOf(c) === s.lastIndexOf(c)) answer.push(c);
   return answer.sort().join("");
 };
