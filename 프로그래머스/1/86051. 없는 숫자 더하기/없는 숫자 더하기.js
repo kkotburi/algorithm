414cdea5285e318f030c4d6933cf3048d662e77b
Hyeyeong Lee <128236313+kkotburi@users.noreply.github.com>
2025-10-24 13:57:31 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -1,9 +1,1 @@
-const solution = (numbers) => {
-  let answer = 0;
-  for (let i = 1; i <= 9; i++) {
-    if (!numbers.includes(i)) {
-      answer += i;
-    }
-  }
-  return answer;
-};
+const solution = (numbers) => 45 - numbers.reduce((cur, acc) => cur + acc, 0);
