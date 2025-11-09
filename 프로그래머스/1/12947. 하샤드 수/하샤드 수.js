efac8f8d7a033912b6f7c6d08a256646b931c108
Hyeyeong Lee <128236313+kkotburi@users.noreply.github.com>
2025-11-09 23:03:11 +0900

    [level 1] Title: 하샤드 수, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.js
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.js
@@ -1,9 +1,1 @@
-function solution(x) {
-  let num = x;
-  let sum = 0;
-  do {
-    sum += x % 10;
-    x = Math.floor(x / 10);
-  } while (x > 0);
-  return !(num % sum);
-}
+const solution = (x) => (x % eval([...x.toString()].join("+")) ? false : true);
