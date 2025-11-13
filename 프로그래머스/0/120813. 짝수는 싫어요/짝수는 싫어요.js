76d2cb7242dd0ce30b2831667dbf12c2d7bbd032
Hyeyeong Lee <128236313+kkotburi@users.noreply.github.com>
2025-11-13 22:37:29 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.js
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.js
@@ -1,5 +1,2 @@
-const solution = (n) => {
-  let answer = [];
-  for (let i = 1; i <= n; i += 2) answer.push(i);
-  return answer;
-};
+const solution = (n) =>
+  Array.from({ length: n }, (_, i) => i + 1).filter((v) => v % 2 !== 0);
