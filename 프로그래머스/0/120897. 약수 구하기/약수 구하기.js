ada9908a90f26235295c4401ed5d300470d58165
Hyeyeong Lee <128236313+kkotburi@users.noreply.github.com>
2025-11-20 15:29:37 +0900

    [level 0] Title: 약수 구하기, Time: 0.73 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/0/120897. 약수 구하기/약수 구하기.js
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.js
@@ -1,9 +1,5 @@
-const solution = (n) => {
-  let answer = [];
-  for (let i = 1; i <= n; i++) {
-    if (!(n % i)) {
-      answer.push(i);
-    }
-  }
-  return answer;
-};
+const solution = (n) =>
+  Array(n)
+    .fill(0)
+    .map((v, i) => v + i + 1)
+    .filter((v) => !(n % v));
