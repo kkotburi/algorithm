ffc6a4561d87db1d2ea08f51b8f6bc56cc4bd43a
Hyeyeong Lee <128236313+kkotburi@users.noreply.github.com>
2025-12-05 21:50:39 +0900

    [level 0] Title: 배열 비교하기, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.js
@@ -0,0 +1,12 @@
+const solution = (arr1, arr2) =>
+  arr1.length === arr2.length
+    ? compare(arr1, arr2, "reduce")
+    : compare(arr1.length, arr2.length);
+
+const compare = (a, b, option) => {
+  if (option === "reduce") {
+    a = a.reduce((acc, cur) => acc + cur);
+    b = b.reduce((acc, cur) => acc + cur);
+  }
+  return a > b ? 1 : a < b ? -1 : 0;
+};
